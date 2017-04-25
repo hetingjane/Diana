f3f629ad028c58b6541de4fa13e4df603a3ca911
Prady <prady@rams.colostate.edu>
2017-04-25 11:36:26 -0600

    Added head recognition to start script

--- start.sh
+++ start.sh
@@ -3,12 +3,16 @@ echo 'logging into blue for fusion'
 gnome-terminal -x bash -c "ssh -t blue 'cd $PWD/fusion/; python fusion_server.py;bash;'" &
 sleep 2
 
+echo 'logging into maserati for Depth RH'
+gnome-terminal -x bash -c "ssh -t maserati 'cd $PWD/handRecognition/; python depth_client.py RH;bash;'" &
+sleep 1
+
 echo 'logging into corvette for Depth LH'
 gnome-terminal -x bash -c "ssh -t corvette 'cd $PWD/handRecognition/; python depth_client.py LH;bash;'" &
 sleep 1
 
-echo 'logging into maserati for Depth RH'
-gnome-terminal -x bash -c "ssh -t maserati 'cd $PWD/handRecognition/; python depth_client.py RH;bash;'" &
+echo 'logging into lotus for Depth Head'
+gnome-terminal -x bash -c "ssh -t lotus 'cd $PWD/headRecognition/; python head_client.py;bash;'" &
 sleep 1
 
 echo 'logging into cyan for skeleton recogntion'
