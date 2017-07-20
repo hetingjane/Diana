5364043a6c34b1082617bdcd4d072804f93797e9
Prady <prady@rams.colostate.edu>
2017-07-20 16:21:02 -0600

    Depth client updated to latest KSIM

--- handRecognition/depth_client.py
+++ handRecognition/depth_client.py
@@ -10,6 +10,8 @@ import numpy as np
 from realtime_hand_recognition import RealTimeHandRecognition
 from support.constants import *
 
+stream_id = 64 | 128;
+
 def connect(server="kinect"):
     """
     Connect to a specific port
@@ -26,11 +28,18 @@ def connect(server="kinect"):
 
     try:
         sock.connect((src_addr, src_port))
-        # Socket will only be used to read, so make it unidirectional
-        #sock.shutdown(socket.SHUT_WR)
     except:
         print "Error connecting to {}:{}".format(src_addr, src_port)
         return None
+
+    if server == "kinect":
+        try:
+            print "Sending stream info"
+            sock.sendall(struct.pack('<i', stream_id));
+        except:
+            print "Error: Stream rejected"
+            return None
+
     print "Successfully connected to host ", server
     return sock
 
@@ -41,42 +50,20 @@ def connect(server="kinect"):
 # right_hand_depth_data ([right_hand_width * right_hand_height])
 def decode_frame(raw_frame):
     # Expect network byte order
-    endianness = "!"
+    endianness = "<"
 
     # In each frame, a header is transmitted
-    header_format = "qiiiii"
+    header_format = "qiiiff"
     header_size = struct.calcsize(endianness + header_format)
     header = struct.unpack(endianness + header_format, raw_frame[:header_size])
 
-    timestamp, depth_hands_count, left_hand_height, left_hand_width, right_hand_height, right_hand_width = header
+    timestamp, frame_type, width, height, posx, posy = header
 
-    left_hand_pos_format = "ffH"
-    left_hand_pos_size = struct.calcsize(left_hand_pos_format)
-    left_hand_pos = struct.unpack(endianness + left_hand_pos_format,
-                                  raw_frame[header_size: header_size + left_hand_pos_size])
+    depth_data_format = str(width * height) + "H"
 
-    right_hand_pos_format = "ffH"
-    right_hand_pos_size = struct.calcsize(right_hand_pos_format)
-    right_hand_pos = struct.unpack(endianness + left_hand_pos_format, raw_frame[
-                                                                      header_size + left_hand_pos_size: header_size + left_hand_pos_size + right_hand_pos_size])
-    depth_hands_size = left_hand_pos_size + right_hand_pos_size
-    depth_hands = left_hand_pos + right_hand_pos
+    depth_data = struct.unpack_from(endianness + depth_data_format, raw_frame, header_size)
 
-    left_hand_depth_data_format = str(left_hand_width * left_hand_height) + "H"
-    right_hand_depth_data_format = str(right_hand_width * right_hand_height) + "H"
-
-    left_hand_depth_data = ()
-    right_hand_depth_data = ()
-
-    if left_hand_width * left_hand_height > 0 and right_hand_height * right_hand_width > 0:
-        depth_data = struct.unpack_from(endianness + left_hand_depth_data_format + right_hand_depth_data_format,
-                                        raw_frame, header_size + depth_hands_size)
-        left_hand_depth_data = depth_data[:left_hand_width * left_hand_height]
-        right_hand_depth_data = depth_data[left_hand_width * left_hand_height:]
-    #print timestamp, depth_hands_count, depth_hands, left_hand_width, left_hand_height, right_hand_width, right_hand_height
-
-    return (timestamp, depth_hands_count) + depth_hands + (
-    left_hand_width, left_hand_height, right_hand_width, right_hand_height, left_hand_depth_data, right_hand_depth_data)
+    return (timestamp, frame_type, width, height, posx, posy, list(depth_data))
 
 
 def recv_all(sock, size):
@@ -89,15 +76,11 @@ def recv_all(sock, size):
     return result
 
 
-def check_savings(raw_frame):
-    return ((len(raw_frame) - len(gzip.compress(raw_frame))) / len(raw_frame)) * 100
-
 def recv_depth_frame(sock):
     """
     Experimental function to read each stream frame from the server
     """
-    (frame_size,) = struct.unpack("!i", recv_all(sock, 4))
-
+    (frame_size,) = struct.unpack("<i", recv_all(sock, 4))
     return recv_all(sock, frame_size)
 
 
@@ -112,6 +95,16 @@ if __name__ == '__main__':
     gestures += ['blind']
     print hand, num_gestures
 
+    if hand == "RH":
+        id = FUSION_RIGHT_HAND_ID
+    else:
+        id = FUSION_LEFT_HAND_ID
+
+    if hand == "LH":
+        stream_id = 64
+    elif hand == "RH":
+        stream_id = 128
+
 
     hand_classfier = RealTimeHandRecognition(hand, num_gestures)
     kinect_socket = connect()
@@ -119,61 +112,31 @@ if __name__ == '__main__':
 
     i = 0
 
-    if hand == "RH":
-        id = FUSION_RIGHT_HAND_ID
-    else:
-        id = FUSION_LEFT_HAND_ID
+
     hands_list = []
 
     start_time = time.time()
     while True:
         try:
             frame = recv_depth_frame(kinect_socket)
-            decoded_frame = decode_frame(frame)
         except KeyboardInterrupt:
            break
         except:
             continue
 
-        timestamp = decoded_frame[0]
+        timestamp, frame_type, width, height, posx, posy, depth_data = decode_frame(frame)
 
-        offset = 2 + (decoded_frame[1] * 2) + 2
-        lwidth = decoded_frame[offset]
-        lheight = decoded_frame[offset + 1]
-        rwidth = decoded_frame[offset + 2]
-        rheight = decoded_frame[offset + 3]
+        hand = np.array(depth_data, dtype=np.float32).reshape((height, width))
+        print hand.shape, posx, posy
+        posz = hand[int(posx), int(posy)]
+        hand = cv2.resize(hand, (168, 168))
+        hand = hand[20:-20, 20:-20]
+        hand -= posz
+        hand /= 150
 
-        left_palm_z = decoded_frame[offset-4]
-        right_palm_z = decoded_frame[offset-1]
-
-
-        if hand == "LH":
-            if decoded_frame[offset + 4]:
-                left_hand = np.array(decoded_frame[offset + 4],dtype=np.float32).reshape((lheight, lwidth))
-                left_hand = cv2.resize(left_hand, (168,168))
-                left_hand = left_hand[20:-20, 20:-20]
-                left_hand -= left_palm_z
-                left_hand /= 150
-                left_hand = left_hand.reshape((1,128,128,1))
-                max_index, probs = hand_classfier.classify(left_hand)
-                probs = list(probs)+[0]
-            else:
-                max_index = num_gestures
-                probs = [0]*num_gestures+[1]
-
-        if hand == "RH":
-            if decoded_frame[offset + 5]:
-                right_hand = np.array(decoded_frame[offset + 5],dtype=np.float32).reshape((rheight, rwidth))
-                right_hand = cv2.resize(right_hand, (168, 168))
-                right_hand = right_hand[20:-20, 20:-20]
-                right_hand -= right_palm_z
-                right_hand /= 150
-                right_hand = right_hand.reshape((1, 128, 128, 1))
-                max_index, probs = hand_classfier.classify(right_hand)
-                probs = list(probs) + [0]
-            else:
-                max_index = num_gestures
-                probs = [0] * num_gestures+[1]
+        hand = hand.reshape((1,128,128,1))
+        max_index, probs = hand_classfier.classify(hand)
+        probs = list(probs)+[0]
 
         print i, timestamp, gestures[max_index], probs[max_index]
         i += 1
