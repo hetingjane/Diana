5f0fb830d3acf0b4064ca575207cfaca2346b655
Rahul Bangar <rahul.bangar@colostate.edu>
2017-07-31 14:10:14 -0600

    Fix: skeleton client handles kinect server exit, use config constants directly, fixed a bug in decoding of skeleton frame

--- skeletonRecognition/apart-together.py
+++ skeletonRecognition/apart-together.py
@@ -1,43 +1,36 @@
-import gzip
 import socket
 import struct
-import time
+import sys
 from collections import deque
 
 import numpy as np
 
-from Compute import (check_engage_disengage)
 from SlidingWindow import sliding_window_dataset
 from WindowProcess import (extract_data, process_window_data, collect_all_results, send_default_values)
 from support.constants import *
 
-src_addr = KINECT_SRC_ADDR
-src_port = KINECT_SKELETON_PORT
-
-des_addr = FUSION_SRC_ADDR
-des_port = FUSION_INPUT_PORT
-
 stream_id = 32
+
 
 def connect():
     """
-    Connect to a specific port
+    Connect to Kinect Server
     """
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
-        sock.connect((src_addr, src_port))
+        sock.connect((KINECT_SRC_ADDR, KINECT_SKELETON_PORT))
     except:
-        print "Error connecting to {}:{}".format(src_addr, src_port)
+        print "Error connecting to {}:{}".format(KINECT_SRC_ADDR, KINECT_SKELETON_PORT)
         return None
     try:
-		print "Sending stream info"
-		sock.sendall(struct.pack('<i', stream_id))
+        print "Sending stream info"
+        sock.sendall(struct.pack('<i', stream_id))
     except:
         print "Error: Stream rejected"
         return None
-    print "Successfully connected to host"
+    print "Successfully connected to Kinect Server at " + str(KINECT_SRC_ADDR) + ":" + str(KINECT_SKELETON_PORT)
     return sock
 
 
@@ -47,10 +40,10 @@ def decode_frame(raw_frame):
     # Expect little endian byte order
     endianness = "<"
 
-    # [ commonTimestamp | frame type | Tracked body count
-    header_format = "qiB"
+    # [ commonTimestamp | frame type | Tracked body count | Engaged
+    header_format = "qiBB"
 
-    timestamp, frame_type, tracked_body_count, enagaged = struct.unpack(endianness + header_format, raw_frame[:struct.calcsize(header_format)])
+    timestamp, frame_type, tracked_body_count, engaged = struct.unpack(endianness + header_format, raw_frame[:struct.calcsize(header_format)])
 
     # For each body, a header is transmitted
     # TrackingId | HandLeftConfidence | HandLeftState | HandRightConfidence | HandRightState ]
@@ -61,13 +54,11 @@ def decode_frame(raw_frame):
     joint_format = "BB7f"
 
     frame_format = body_format + (joint_format * 25)
-
-    tracked = tracked_body_count > 0
     
     # Unpack the raw frame into individual pieces of data as a tuple
-    frame_pieces = struct.unpack(endianness + (frame_format * (1 if tracked else 0)), raw_frame[struct.calcsize(header_format):])
+    frame_pieces = struct.unpack(endianness + (frame_format * engaged), raw_frame[struct.calcsize(header_format):])
     
-    decoded = (timestamp, frame_type, tracked_body_count, enagaged) + frame_pieces
+    decoded = (timestamp, frame_type, tracked_body_count, engaged) + frame_pieces
 
     return decoded
 
@@ -87,7 +78,7 @@ def recv_skeleton_frame(sock):
     To read each stream frame from the server
     """
     (load_size,) = struct.unpack("<i", recv_all(sock, struct.calcsize("<i")))
-    #print load_size
+    # print load_size
     return recv_all(sock, load_size)
 
 
@@ -98,12 +89,12 @@ def connect_fusion():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
-        sock.connect((des_addr, des_port))
+        sock.connect((FUSION_SRC_ADDR, FUSION_INPUT_PORT))
         # Socket will only be used to read, so make it unidirectional
     except:
-        print "Error connecting to {}:{}".format(des_addr, des_port)
+        print "Error connecting to {}:{}".format(FUSION_SRC_ADDR, FUSION_INPUT_PORT)
         return None
-    print "Successfully connected to host"
+    print "Successfully connected to Fusion Server at " + str(FUSION_SRC_ADDR) + ":" + str(FUSION_INPUT_PORT)
     return sock
 
 
@@ -114,8 +105,8 @@ if __name__ == '__main__':
     s = connect()
     r = connect_fusion()
 
-    if s==None:
-	sys.exit(0)
+    if s is None:
+        sys.exit(0)
 
     window_threshold = 15
     body_parts = ['LA', 'RA']
@@ -128,14 +119,18 @@ if __name__ == '__main__':
     arms_y_bits = ["still", "stack up", "stack down"]
 
     while True:
-        f = recv_skeleton_frame(s)
+        try:
+            f = recv_skeleton_frame(s)
+        except EOFError:
+            print "Disconnected from Kinect Server"
+            break
         fd = decode_frame(f)
-        timestamp, frame_type, body_count, enagaged = fd[:4]
-        input_data = (timestamp, body_count) + fd[4:]
-  
-        data_stream.extend([input_data])
+        timestamp, frame_type, body_count, engaged = fd[:4]
+
         if engaged:
-            if (len(data_stream) >= window_threshold):
+            input_data = (timestamp, body_count) + fd[4:]
+            data_stream.extend([input_data])
+            if len(data_stream) >= window_threshold:
                 t = np.vstack([extract_data(frame) for frame in data_stream])
                 test_window = sliding_window_dataset([t], window_threshold)
 
@@ -144,26 +139,26 @@ if __name__ == '__main__':
                     res = process_window_data(test_window, body_part=b)
                     proba_array.append(res[0].tolist()), map_array.append(res[1])
 
-                #Processing arms apart and arms together -X direction
+                # Processing arms apart and arms together -X direction
                 if (map_array[0] == 0 and map_array[1] == 1) or (map_array[0] == 1 and map_array[1] == 0):
                     _, bit_val = process_window_data(test_window, body_part='arms_x')
 
-                    if (bit_val == 0):
+                    if bit_val == 0:
                         bit_val = 26
-                    elif (bit_val == 1):
+                    elif bit_val == 1:
                         bit_val = 27
-                    elif (bit_val == 2):
+                    elif bit_val == 2:
                         bit_val = 28
                     map_array[0] = map_array[1] = bit_val
 
-                #Processing arms apart and arms together -Y direction
+                # Processing arms apart and arms together -Y direction
                 if (map_array[0] == 2 and map_array[1] == 3) or (map_array[0] == 3 and map_array[1] == 2):
                     _, bit_val = process_window_data(test_window, body_part='arms_y')
-                    if (bit_val == 0):
+                    if bit_val == 0:
                         bit_val = 26
-                    elif (bit_val == 1):
+                    elif bit_val == 1:
                         bit_val = 29
-                    elif (bit_val == 2):
+                    elif bit_val == 2:
                         bit_val = 30
                     map_array[0] = map_array[1] = bit_val
 
@@ -171,24 +166,24 @@ if __name__ == '__main__':
                 map_array, proba_array = send_default_values(body_parts)
             result = collect_all_results(map_array, proba_array, int(engaged))
 
-
         else:
             map_array, proba_array = send_default_values(body_parts)
             result = collect_all_results(map_array, proba_array, int(engaged))
 
-        time_stamp = list(data_stream)[-1][0]
-        print time_stamp, (result[:2]), body_count
-        pack_list = [1, time_stamp] + result
-        bytes = struct.pack("!iqii" + "ff" * 6 + 'i', *pack_list)
+        if len(data_stream) > 0:
+            time_stamp = list(data_stream)[-1][0]
+            print time_stamp, (result[:2]), body_count, engaged
+            pack_list = [1, time_stamp] + result
+            raw_data = struct.pack("!iqii" + "ff" * 6 + 'i', *pack_list)
 
-        if r != None:
-            r.sendall(bytes)    
+            if r is not None:
+                r.sendall(raw_data)
 
     print "Total frame time: {}".format(avg_frame_time)
-
    
     s.close()
-    if r != None:
+    if r is not None:
         r.close()
+
     sys.exit(0)
 
