b0ed740cf290c57849d45b6964ba42f294e89d42
Rahul Bangar <rahul.bangar@colostate.edu>
2017-11-01 13:48:06 -0600

    Modified tests to be more uniform

--- fusion/tests/test_tri_state_machines.py
+++ fusion/tests/test_tri_state_machines.py
@@ -18,12 +18,16 @@ posack_non_trigger = posture_to_vec['engage'] | posture_to_vec['rh thumbs down']
 posack_high, posack_low, posack_stop = get_high_low_stop(posack_trigger, posack_non_trigger)
 
 @pytest.mark.parametrize("test_input, expected_output", [
-    # base
-    ([], ['stop']),
+    # Excess stops do not trigger a transition from initial state
+    ([posack_stop] * 20, ['stop']),
+    # Excess lows trigger a transition to low
+    ([posack_low] * 8, ['stop', 'low']),
+    # Excess highs trigger a transition to low
+    ([posack_high] * 8, ['stop', 'high']),
     # Tests the transition stop -> high -> low -> high -> stop
     ([posack_high]*5 + [posack_low]*5 + [posack_high]*5 + [posack_stop]*5, ['stop', 'high', 'low', 'high', 'stop']),
-    # Test the transition stop -> partial high -> low -> partial high -> stop
-    ([posack_high]*2 + [posack_low]*5 + [posack_high]*3 + [posack_stop]*5, ['stop', 'low', 'stop']),
+    # Tests the transition stop -> low -> high -> low -> stop
+    ([posack_low] * 5 + [posack_high] * 5 + [posack_low] * 5 + [posack_stop] * 5, ['stop', 'low', 'high', 'low', 'stop']),
     # Test the transition stop -> excess high -> partial low -> partial high -> stop
     ([posack_low]*3 + [posack_high]*8 + [posack_low] * 4 + [posack_high]*2 + [posack_stop]*5, ['stop', 'high', 'stop']),
     # Test the transition stop -> excess high -> disengage once -> partial high
@@ -42,15 +46,19 @@ point_non_trigger = posture_to_vec['engage'] | posture_to_vec['rh thumbs down']
 
 point_high, point_low, point_stop = get_high_low_stop(point_trigger, point_non_trigger)
 
-
-
 @pytest.mark.parametrize("test_input, expected_output", [
-    # base
-    ([], ['stop']),
+    # Excess stops do not trigger a transition from initial state
     ([point_stop]*20, ['stop']),
+    # Excess lows trigger a transition to low
     ([point_low]*8, ['stop', 'low']),
-    # point -> move -> point
-    ([point_high]*6 + [point_low]*8 + [point_high]*5, ['stop', 'high', 'low', 'high'])
+    # Excess highs trigger a transition to low
+    ([point_high]*8, ['stop', 'high']),
+    # Tests the transition stop -> high -> low -> high -> stop
+    ([point_high] * 5 + [point_low] * 5 + [point_high] * 5 + [point_stop] * 5, ['stop', 'high', 'low', 'high', 'stop']),
+    # Tests the transition stop -> low -> high -> low -> stop
+    ([point_low] * 5 + [point_high] * 5 + [point_low] * 5 + [point_stop] * 5, ['stop', 'low', 'high', 'low', 'stop']),
+    # Test the transition stop -> excess high -> disengage once -> partial high
+    ([point_high]*6 + [disengage] + [point_high]*4, ['stop', 'high', 'stop'])
 ])
 def test_point(test_input, expected_output):
     tsm_right_point_vec.reset()
