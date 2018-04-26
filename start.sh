5f15b7ba615630938663b525eb930f4bf052a531
Rahul Bangar <rahul.bangar@colostate.edu>
2018-04-26 13:52:02 -0600

    start.sh deals with arguments using getopt now

--- start.sh
+++ start.sh
@@ -1,75 +1,129 @@
 #!/usr/bin/env bash
 
-START_DIR=$(dirname "$0")
-START_DIR=$(realpath "$START_DIR")
+function print_usage
+{
+    echo -e "\nUsage: start.sh [-h|--help] [-e|--env <virtual_env>] [-c|--conf <machine_specification> default:machines.bak] [-s|--single-machine default:no]\n"
+}
+    
+# full path to directory where start.sh resides
+start_dir=$(dirname "$0")
+start_dir=$(realpath "$start_dir")
 
-if [ -d "$1" ]
+# parse arguments with getopt
+my_args=$(getopt -o he:c:s -l help,env:,conf:,single-machine -n 'start.sh' -- "$@")
+eval set -- "$my_args"
+
+# default values
+env_dir=""
+single_machine=no
+machine_spec="$start_dir/machines.bak"
+
+while true
+do
+    case "$1" in
+    
+        -h|--help)
+            print_usage
+            exit 0
+            ;;
+        
+        -e|--env)
+            if [ -d "$2" ]
+            then
+                env_dir="$start_dir/$2"
+                shift 2
+            else
+                echo "Error: virtual environment $start_dir/$2 does not exist"
+                exit 1
+            fi
+            ;;
+            
+        -c|--conf)
+            if [ -e "$2" ]
+            then
+                machine_spec="$start_dir/$2"
+                shift 2
+            else
+                echo "Error: machine specification $start_dir/$2 does not exist"
+                exit 1
+            fi
+            ;;
+            
+        -s|--single-machine)
+            single_machine=yes ; shift ;;
+        
+        --) shift ; break ;;
+        
+        *) print_usage ; exit 1 ;;
+    esac
+done
+
+
+if [ "$env_dir" = "" ]
 then
-    VENV_DIR=$(realpath "$1")
-    echo -e "Using virtualenv directory: ${VENV_DIR}\n"
+    echo "Virtual environment: none (user/system packages)"
 else
-    echo -e "No valid virtualenv directory specified. Using system environement.\nTo specify a virtualenv directory: $0 <venv_dir>\n"
-    VENV_DIR="x"
+    echo "Virtual environment: $env_dir"
 fi
 
-target="machines"
+echo "Single machine: $single_machine"
+echo "Machine spec: $machine_spec"
 
-if [ ! -e "$target" ]; then
-    target="machines.bak"
-fi
+echo ""
 
 device=0
 params=""
-for i in $(grep -e '^[^#].*' $target)
+for i in $(grep -e '^[^#].*' $machine_spec)
 do
     process=${i%@*}
     machine=${i#*@}
     if [ "$machine" = "local" ]; then
         machine="$HOSTNAME"
     fi
+    
     case "$process" in
-    "fusion")
-    params="$params --tab -e \"ssh -t ${machine} 'cd ${START_DIR}; if [ ${VENV_DIR} != 'x' ]; then source ${VENV_DIR}/bin/activate; fi; python -m components.fusion.fusion_server; bash;'\" --title ${i}"
-    ;;
-    "lh")
-    params="$params --tab -e \"ssh -t ${machine} 'cd ${START_DIR}; export CUDA_VISIBLE_DEVICES=${device}; if [ ${VENV_DIR} != 'x' ]; then source ${VENV_DIR}/bin/activate; fi; python -m components.handRecognition.depth_client LH; bash;'\" --title ${i}"
-    if [ "$2" = "--single-machine" ]
-    then
-        ((device++))
-    fi
-    ;;
-    "rh")
-    params="$params --tab -e \"ssh -t ${machine} 'cd ${START_DIR}; export CUDA_VISIBLE_DEVICES=${device}; if [ ${VENV_DIR} != 'x' ]; then source ${VENV_DIR}/bin/activate; fi; python -m components.handRecognition.depth_client RH; bash;'\" --title ${i}"
-    if [ "$2" = "--single-machine" ]
-    then
-        ((device++))
-    fi
-    ;;
-    "head")
-    params="$params --tab -e \"ssh -t ${machine} 'cd ${START_DIR}; export CUDA_VISIBLE_DEVICES=${device}; if [ ${VENV_DIR} != 'x' ]; then source ${VENV_DIR}/bin/activate; fi; python -m components.headRecognition.head_client; bash;'\" --title ${i}"
-    if [ "$2" = "--single-machine" ]
-    then
-        ((device++))
-    fi
-    ;;
-    "speech")
-    params="$params --tab -e \"ssh -t ${machine} 'cd ${START_DIR}; sleep 3; python -m components.speech.speech_client; bash;'\" --title ${i}"
-    ;;
-    "body")
-    params="$params --tab -e \"ssh -t ${machine} 'cd ${START_DIR}; sleep 3; export CUDA_VISIBLE_DEVICES=${device}; if [ ${VENV_DIR} != 'x' ]; then source ${VENV_DIR}/bin/activate; fi; python -m components.skeletonRecognition.body_client; bash;'\" --title ${i}"
-    if [ "$2" = "--single-machine" ]
-    then
-        ((device++))
-    fi
-    ;;
-    *)
-    echo "Invalid process specified: ${process}"
-    exit
-    ;;
+        "fusion")
+            params="$params --tab -e \"ssh -t ${machine} 'cd ${start_dir}; if [ ! -z ${env_dir} ]; then source ${env_dir}/bin/activate; fi; python -m components.fusion.fusion_server; bash;'\" --title ${i}"
+            ;;
+        "lh")
+            params="$params --tab -e \"ssh -t ${machine} 'cd ${start_dir}; export CUDA_VISIBLE_DEVICES=${device}; if [ ! -z ${env_dir} ]; then source ${env_dir}/bin/activate; fi; python -m components.handRecognition.depth_client LH; bash;'\" --title ${i}"
+            if [ "$single_machine" = yes ]
+            then
+                ((device++))
+            fi
+            ;;
+        "rh")
+            params="$params --tab -e \"ssh -t ${machine} 'cd ${start_dir}; export CUDA_VISIBLE_DEVICES=${device}; if [ ! -z ${env_dir} ]; then source ${env_dir}/bin/activate; fi; python -m components.handRecognition.depth_client RH; bash;'\" --title ${i}"
+            if [ "$single_machine" = yes ]
+            then
+                ((device++))
+            fi
+            ;;
+        "head")
+            params="$params --tab -e \"ssh -t ${machine} 'cd ${start_dir}; export CUDA_VISIBLE_DEVICES=${device}; if [ ! -z ${env_dir} ]; then source ${env_dir}/bin/activate; fi; python -m components.headRecognition.head_client; bash;'\" --title ${i}"
+            if [ "$single_machine" = yes ]
+            then
+                ((device++))
+            fi
+            ;;
+        "speech")
+            params="$params --tab -e \"ssh -t ${machine} 'cd ${start_dir}; sleep 3; python -m components.speech.speech_client; bash;'\" --title ${i}"
+            ;;
+        "body")
+            params="$params --tab -e \"ssh -t ${machine} 'cd ${start_dir}; sleep 3; export CUDA_VISIBLE_DEVICES=${device}; if [ ! -z ${env_dir} ]; then source ${env_dir}/bin/activate; fi; python -m components.skeletonRecognition.body_client; bash;'\" --title ${i}"
+            if [ "$single_machine" = yes ]
+            then
+                ((device++))
+            fi
+            ;;
+        *)
+            echo "Invalid process specified: ${process}"
+            exit
+            ;;
     esac
 done
 
 cmd="xfce4-terminal ${params}"
 cmd=${cmd/--tab/}
-#echo "$cmd"
+echo "$cmd"
 eval $cmd
