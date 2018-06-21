d8bb89a5f1d8f884a18f2d145f1481967acacfa3
Rahul Bangar <bangar1991@gmail.com>
2018-06-21 14:41:26 -0600

    Port resnet_main.py to py3 partially (missing 'image_reader' module)

--- components/rgbHandRecognition/resnet_main.py
+++ components/rgbHandRecognition/resnet_main.py
@@ -1,10 +1,12 @@
-from resnet_v2 import resnet_v2_50
-import tensorflow as tf
 import os
-import numpy as np
-import resnet_utils
 import time
+
+import tensorflow as tf
+import numpy as np
+
 from image_reader import get_input
+from .resnet_v2 import resnet_v2_50
+from . import resnet_utils
 
 slim = tf.contrib.slim
 
@@ -17,7 +19,7 @@ hand = FLAGS.hand
 log_root = "/s/red/a/nobackup/cwc/tf/hands_demo_rgb/%s_fine" % hand
 train_dir = "/s/red/a/nobackup/cwc/tf/hands_demo_rgb/%s_fine/train" % hand
 eval_dir = "/s/red/a/nobackup/cwc/tf/hands_demo_rgb/%s_fine/eval" % hand
-print log_root, train_dir, eval_dir
+print(log_root, train_dir, eval_dir)
 
 #eval_batch_count = 250611  # 191231
 
@@ -39,7 +41,7 @@ def train():
 
     with slim.arg_scope(resnet_utils.resnet_arg_scope()):
         logits, end_points = resnet_v2_50(images, 20)
-        print logits, end_points
+        print(logits, end_points)
 
     variables_to_restore = slim.get_variables_to_restore()
 
@@ -87,7 +89,7 @@ def train():
     var = [v for v in tf.model_variables() if "50/conv1" in v.name or "logits" in v.name]
     var_list = sess.run(var)
     for v1, v in zip(var, var_list):
-        print v1.name, v.shape, np.min(v), np.max(v), np.mean(v), np.std(v)
+        print(v1.name, v.shape, np.min(v), np.max(v), np.mean(v), np.std(v))
 
     saver.save(sess, os.path.join(log_root,"model.ckpt"), global_step=global_step_1)
     checkpoint_time = time.time()
@@ -112,7 +114,7 @@ def evaluate():
     images, one_hot_labels = get_input(20, FLAGS.batch_size, FLAGS.mode, FLAGS.hand)
     gesture_list = os.listdir("/s/red/a/nobackup/cwc/hands/rgb_hands_new_frames/s_01")
     gesture_list.sort()
-    print gesture_list
+    print(gesture_list)
 
 
     with slim.arg_scope(resnet_utils.resnet_arg_scope()):
@@ -134,7 +136,7 @@ def evaluate():
     
     checkpoint_list = os.listdir(log_root)
     checkpoint_list=[c.replace(".index","") for c in checkpoint_list if ".index" in c]
-    print checkpoint_list
+    print(checkpoint_list)
 
     precision_list = []
 
@@ -172,8 +174,8 @@ def evaluate():
             total_prediction += predictions.shape[0]
 
             if sample % 40 == 0:
-                print sample, correct_prediction, total_prediction
-	    
+                print(sample, correct_prediction, total_prediction)
+
 
         gt = np.squeeze(np.vstack(gt_list))
         pred = np.squeeze(np.vstack(pred_list))
@@ -181,24 +183,29 @@ def evaluate():
 
         
         precision = np.mean(np.argmax(gt,axis=1)==np.argmax(pred,axis=1))
-	precision_list.append(precision)
+        precision_list.append(precision)
         best_precision = max(precision, best_precision)
-	print gt.shape, pred.shape, precision, best_precision
-	gt = np.argmax(gt,axis=1)
-	pred = np.argmax(pred,axis=1)
-	'''cm = np.float(confusion_matrix(gt,pred))
-	
-	cm /= np.sum(cm,axis=0)
-	plt.imshow(cm,interpolation='nearest')
-	plt.xticks(range(20),gesture_list,rotation=90)
-	plt.yticks(range(20),gesture_list)
-	plt.show()
+
+        print(gt.shape, pred.shape, precision, best_precision)
+
+        gt = np.argmax(gt,axis=1)
+        pred = np.argmax(pred,axis=1)
+
+        '''
+        cm = np.float(confusion_matrix(gt, pred))
+        
+        cm /= np.sum(cm,axis=0)
+        plt.imshow(cm,interpolation='nearest')
+        plt.xticks(range(20),gesture_list,rotation=90)
+        plt.yticks(range(20),gesture_list)
+        plt.show()
         precision_summ = tf.Summary()
         precision_summ.value.add(tag='Precision', simple_value=precision)
         summary_writer.add_summary(precision_summ, train_step)
         best_precision_summ = tf.Summary()
         best_precision_summ.value.add(tag='Best Precision', simple_value=best_precision)
-        summary_writer.add_summary(best_precision_summ, train_step)'''
+        summary_writer.add_summary(best_precision_summ, train_step)
+        '''
 
        
         tf.logging.info(
@@ -207,25 +214,25 @@ def evaluate():
         summary_writer.flush()
 
         #time.sleep(60)
-    print "Max Precision : ",np.max(precision_list)," Ckpt: ",checkpoint_list[np.argmax(precision_list)]
+    print("Max Precision : ",np.max(precision_list)," Ckpt: ",checkpoint_list[np.argmax(precision_list)])
+
 
 def forward():
     import random
-    import cv2
+    from skimage.transform import resize
     import matplotlib.pyplot as plt
 
     gesture_list = os.listdir("/s/red/a/nobackup/cwc/hands/rgb_hands_new_frames/s_01")
     gesture_list.sort()
-    print gesture_list
+    print(gesture_list)
 
     #lh_list = np.load("/s/red/a/nobackup/cwc/hands/rgb_test/LH.npy")
     if FLAGS.hand == "RH":
-    	rh_list = np.load("/s/red/a/nobackup/cwc/hands/rgb_test/LH.npy")
-	ckpt = "/s/red/a/nobackup/cwc/tf/hands_demo_rgb/RH_fine/model.ckpt-5590"
+        rh_list = np.load("/s/red/a/nobackup/cwc/hands/rgb_test/LH.npy")
+        ckpt = "/s/red/a/nobackup/cwc/tf/hands_demo_rgb/RH_fine/model.ckpt-5590"
     else:
-	rh_list = np.load("/s/red/a/nobackup/cwc/hands/rgb_test/RH.npy")
-	ckpt = "/s/red/a/nobackup/cwc/tf/hands_demo_rgb/LH_fine/model.ckpt-40734"
-
+        rh_list = np.load("/s/red/a/nobackup/cwc/hands/rgb_test/RH.npy")
+        ckpt = "/s/red/a/nobackup/cwc/tf/hands_demo_rgb/LH_fine/model.ckpt-40734"
 
 
     checkpoint_file = tf.train.latest_checkpoint(log_root)
@@ -238,7 +245,7 @@ def forward():
 
     with slim.arg_scope(resnet_utils.resnet_arg_scope()):
         logits, end_points = resnet_v2_50(_standardized_images, 20)
-        print logits, end_points
+        print(logits, end_points)
 
     variables_to_restore = slim.get_variables_to_restore()
 
@@ -252,21 +259,17 @@ def forward():
     saver.restore(sess, ckpt)
 
     for i in range(10):
-	index = random.randint(0, rh_list.shape[0]-1)
-	rh_image = rh_list[index]
-	rh_image = rh_image[:,:,[2,1,0]]
-        rh_image = cv2.resize(rh_image, (128, 128))
+        index = random.randint(0, rh_list.shape[0]-1)
+        rh_image = rh_list[index]
+        rh_image = rh_image[:,:,[2,1,0]]
+        rh_image = resize(rh_image, (128, 128))
+        print(rh_image.shape)
 
-	print rh_image.shape
-
-	pred_index = sess.run(predictions,feed_dict={_image:rh_image})
-	pred_gesture = gesture_list[pred_index[0]]
-	plt.imshow(rh_image)
-	plt.title(pred_gesture)
-	plt.show()
-
-
-
+        pred_index = sess.run(predictions, feed_dict={_image:rh_image})
+        pred_gesture = gesture_list[pred_index[0]]
+        plt.imshow(rh_image)
+        plt.title(pred_gesture)
+        plt.show()
 
 
 def main(_):
@@ -274,13 +277,14 @@ def main(_):
         FLAGS.batch_size = 64
         train()
     elif FLAGS.mode == "eval":
-	FLAGS.batch_size = 64
+        FLAGS.batch_size = 64
         evaluate()
     else:
-	forward()
+        forward()
 
 #LH: 40734 79.1
 #RH 23593 76.7; 5590 78.83
+
 
 if __name__ == '__main__':
     tf.logging.set_verbosity(tf.logging.INFO)
