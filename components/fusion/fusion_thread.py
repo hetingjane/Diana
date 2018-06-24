70b3c02a94ea9fa196a3ee9ba6671798c9b64c29
Rahul Bangar <bangar1991@gmail.com>
2018-06-24 03:13:38 -0600

    Port fusion_thread to use selectors module

--- components/fusion/fusion_thread.py
+++ components/fusion/fusion_thread.py
@@ -1,8 +1,8 @@
-import select
 import socket
 import struct
 import threading
 from collections import namedtuple
+import selectors
 
 from .conf import streams
 from .conf.postures import right_hand_postures, head_postures
@@ -11,8 +11,6 @@ from .thread_sync import synced_msgs
 
 
 class Fusion(threading.Thread):
-
-    _connected_clients = {}
 
     Header = namedtuple('Header', ['id', 'timestamp', 'name'])
 
@@ -35,6 +33,8 @@ class Fusion(threading.Thread):
         self._msgs_received = {}
         self._stop = threading.Event()
         self._synced = False
+        self._sel = selectors.DefaultSelector()
+        self._connected_streams = {}
 
     def _recv_all(self, sock, size):
         result = b''
@@ -81,6 +81,12 @@ class Fusion(threading.Thread):
             raise ValueError('hand must be either LH or RH: ' + hand)
         return Fusion.HandData(hand_data[0], hand_data[1:], hand_type)
 
+    def _read_lhand_data(self, sock):
+        return self._read_hands_data(sock, 'LH')
+
+    def _read_rhand_data(self, sock):
+        return self._read_hands_data(sock, 'RH')
+
     def _read_head_data(self, sock):
         data_format = "<" + "i" + "f" * len(head_postures)
         raw_data = self._recv_all(sock, struct.calcsize(data_format))
@@ -96,8 +102,7 @@ class Fusion(threading.Thread):
         command = command.decode('ascii')
         return Fusion.SpeechData(command)
 
-    def _read_stream_data(self, sock, stream_id):
-        stream_name = streams.get_stream_name(stream_id)
+    def _read_stream_data(self, sock, stream_name):
         if stream_name in ["LH", "RH"]:
             return self._read_hands_data(sock, stream_name)
         elif stream_name == "Body":
@@ -106,11 +111,6 @@ class Fusion(threading.Thread):
             return self._read_head_data(sock)
         elif stream_name == "Speech":
             return self._read_speech_data(sock)
-
-    def _handle_client(self, sock):
-        header = self._read_stream_header(sock)
-        data = self._read_stream_data(sock, header.id)
-        return Fusion.Message(header, data)
 
     def _set_sync(self, sync_ts):
         if not self._synced:
@@ -136,119 +136,112 @@ class Fusion(threading.Thread):
     def is_stopped(self):
         return self._stop.is_set()
 
-    def _accept_stream(self, sock, addr):
+    def _handle_client(self, key):
+        conn = key.fileobj
         try:
-            stream_id_bytes = self._recv_all(sock, 4)
-            stream_id = struct.unpack('<i', stream_id_bytes)[0]
-        except Exception:
+            header = self._read_stream_header(conn)
+            data = self._read_stream_data(conn, header.name)
+            return Fusion.Message(header, data)
+        except (socket.error, EOFError):
+            print("Client disconnected")
+            self._unset_sync()
+            self._sel.unregister(conn)
+            conn.close()
+            self._connected_streams.pop(conn)
+            return None
+
+    def _accept_stream(self, key):
+        try:
+            conn, addr = key.fileobj.accept()
+        except socket.error:
+            print("Error while accepting connection")
+            return
+
+        try:
+            stream_id_bytes = self._recv_all(conn, 4)
+        except socket.error:
             print("Unable to receive complete stream id. Ignoring the client")
-            sock.close()
+            return
+
+        stream_id = struct.unpack('<i', stream_id_bytes)[0]
         print("Received stream id. Verifying...")
+
         if streams.is_valid_id(stream_id):
             stream_name = streams.get_stream_name(stream_id)
+
             if streams.is_active(stream_name):
                 print("Stream is valid and active: {}".format(stream_name))
-                print("Checking if stream is already connected...")
-                if stream_name not in self._connected_clients.values():
-                    print("New stream. Accepting the connection {host[0]}:{host[1]}".format(host=addr))
-                    sock.shutdown(socket.SHUT_WR)
-                    self._connected_clients[sock] = stream_name
-                    return True
+                if stream_name not in self._connected_streams.values():
+                    self._connected_streams[conn] = stream_name
+                    print("Accepting the connection {host[0]}:{host[1]}".format(host=addr))
+                    self._sel.register(conn, selectors.EVENT_READ, data=self._handle_client)
                 else:
-                    print("Stream already exists. Rejecting the connection")
-                    sock.close()
+                    print("Stream already connected. Rejecting duplicate stream")
+                    conn.close()
             else:
                 print("Rejecting inactive stream: {}".format(stream_name))
-                sock.close()
+                conn.close()
         else:
             print("Rejecting invalid stream with id: {}".format(stream_id))
-            sock.close()
-        return False
+            conn.close()
 
     def run(self):
         serv_sock = serve('fusion')
         serv_sock.listen(5)
 
-        inputs = [serv_sock]
-        outputs = []
-        excepts = []
+        self._sel.register(serv_sock, selectors.EVENT_READ, data=self._accept_stream)
 
         print("Waiting for clients to connect")
 
         while not self.is_stopped():
-            try:
-                read_socks, write_socks, except_socks = select.select(inputs, outputs, excepts, 0.01)
-            except socket.error:
-                for sock in inputs:
-                    try:
-                        select.select([sock], [], [], 0)
-                    except Exception:
-                        print("Client disconnected")
-                        inputs.remove(sock)
-                        self._connected_clients.pop(sock)
-                        self._unset_sync()
-                continue
+            events = self._sel.select()
+            for key, mask in events:
+                handler = key.data
+                msg = handler(key)
 
-            for s in read_socks:
-                if s is serv_sock:
-                    client_sock, client_addr = s.accept()
-                    # 1 is for the server socket
-                    if self._accept_stream(client_sock, client_addr):
-                        inputs += [client_sock]
-                else:
-                    try:
-                        msg = self._handle_client(s)
-                    except (socket.error, EOFError):
-                        print("Client disconnected")
-                        inputs.remove(s)
-                        self._connected_clients.pop(s)
-                        self._unset_sync()
-                        continue
+                if msg and streams.all_connected(self._connected_streams.values()):
+                    cur_ts = msg.header.timestamp
 
-                    # Read and discard data unless enough clients connect
-                    if streams.all_connected(self._connected_clients.values()):
-                        cur_ts = msg.header.timestamp
+                    # Add data to appropriate timestamp bucket
+                    self._msgs_received.setdefault(cur_ts, []).append(msg)
 
-                        # Add data to appropriate timestamp bucket
-                        self._msgs_received.setdefault(cur_ts, []).append(msg)
-
-                        # Try to sync in presence of this new data
-                        # Will run every time until we are synced
-                        if not self._is_synced():
-                            # Try to find a sync point if it exists
-                            for ts in sorted(self._msgs_received.keys()):
-                                # Weak check for all data received
-                                if len(self._msgs_received[ts]) == streams.get_active_streams_count():
-                                    # This is the sync point
-                                    sync_ts = ts
-                                    self._set_sync(sync_ts)
-                                    break
+                    # Try to sync in presence of this new data
+                    # Will run every time until we are synced
+                    if not self._is_synced():
+                        # Try to find a sync point if it exists
+                        for ts in sorted(self._msgs_received.keys()):
+                            # Weak check for all data received
+                            if len(self._msgs_received[ts]) == streams.get_active_streams_count():
+                                # This is the sync point
+                                sync_ts = ts
+                                self._set_sync(sync_ts)
+                                break
+                    else:
+                        # Already synced
+                        sync_ts = min(self._msgs_received.keys())
+                        # print "Minimum timestamp: {0}".format(sync_ts)
+                        if len(self._msgs_received[sync_ts]) == streams.get_active_streams_count():
+                            # Create a shared data object representing the synced data
+                            # Indexed by stream type
+                            # Value is the entire decoded frame of that stream type
+                            # print "Timestamp contains all data"
+                            s_msg = {}
+                            for m in self._msgs_received[sync_ts]:
+                                s_msg[m.header.name] = m
+                            # print "{0:d}, LH: {1:.2f}, {2:.2f}, RH: {3:.2f}, {4:.2f}".format(sync_ts,
+                            # s_msg["Body"].data.pos_l_x, s_msg["Body"].data.pos_l_y,
+                            # s_msg["Body"].data.pos_r_x, s_msg["Body"].data.pos_r_x)
+                            synced_msgs.put(s_msg)
+                            self._msgs_received.pop(sync_ts)
+                        """
                         else:
-                            # Already synced
-                            sync_ts = min(self._msgs_received.keys())
-                            #print "Minimum timestamp: {0}".format(sync_ts)
-                            if len(self._msgs_received[sync_ts]) == streams.get_active_streams_count():
-                                # Create a shared data object representing the synced data
-                                # Indexed by stream type
-                                # Value is the entire decoded frame of that stream type
-                                #print "Timestamp contains all data"
-                                s_msg = {}
-                                for m in self._msgs_received[sync_ts]:
-                                    s_msg[m.header.name] = m
-                                #print "{0:d}, LH: {1:.2f}, {2:.2f}, RH: {3:.2f}, {4:.2f}".format(sync_ts,
-                                # s_msg["Body"].data.pos_l_x, s_msg["Body"].data.pos_l_y,
-                                # s_msg["Body"].data.pos_r_x, s_msg["Body"].data.pos_r_x)
-                                synced_msgs.put(s_msg)
-                                self._msgs_received.pop(sync_ts)
-                            """
-                            else:
-                                found_streams = []
-                                for m in self._msgs_received[sync_ts]:
-                                    found_streams += [m.header.name]
-                                print "Only " + str(len(found_streams)) + " streams were found: " + ",".join(found_streams)
-                            """
+                            found_streams = []
+                            for m in self._msgs_received[sync_ts]:
+                                found_streams += [m.header.name]
+                            print "Only " + str(len(found_streams)) + " streams were found: " + ",".join(found_streams)
+                        """
 
         print("Stopped network thread")
 
-        for s in inputs:
-            s.close()
+        for conn in self._sel.get_map():
+            conn.close()
