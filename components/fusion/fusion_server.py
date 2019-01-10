ffd9c8ed69efdc3dab9537ad99ee1697fa22651b
Rahul Bangar <rahul.bangar@colostate.edu>
2019-01-10 14:06:30 -0700

    Add teaching state machine on startup

--- components/fusion/fusion_server.py
+++ components/fusion/fusion_server.py
@@ -294,7 +294,8 @@ brandeis_events = [machines.engage, machines.wave,
                    machines.left_point, machines.right_point,
                    machines.left_point_continuous, machines.right_point_continuous,
                    machines.push_left, machines.push_right, machines.push_front, machines.push_back,
-                   machines.grab, machines.push_servo_left, machines.push_servo_right]
+                   machines.grab, machines.push_servo_left, machines.push_servo_right,
+                   machines.teaching]
 
 brandeis_events += create_one_shot_learning_events(5)
 
