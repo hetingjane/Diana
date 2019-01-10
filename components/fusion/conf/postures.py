9ecd4aa473cef213bf45288dbcd78150c359928a
zyu47 <yuzhixian1990@gmail.com>
2019-01-10 12:17:06 -0700

    add new gesture and learning posture

--- components/fusion/conf/postures.py
+++ components/fusion/conf/postures.py
@@ -6,7 +6,9 @@ left_hand_postures = ['lh blank', 'rh hands together', 'lh other', 'lh beckon', 
                       'lh open back', 'lh open down', 'lh open right', 'lh point down',
                       'lh point front', 'lh point right', 'lh stop', 'lh three back',
                       'lh three front', 'lh thumbs down', 'lh thumbs up', 'lh to face',
-                      'lh two back', 'lh two front', 'lh blind', 'lh grab cup']
+                      'lh two back', 'lh two front', 'lh new gesture 1', 'lh new gesture 2',
+                      'lh new gesture 3', 'lh new gesture 4', 'lh new gesture 5', 'lh learning',
+                      'lh learned', 'lh blind']
 
 # Make sure to change depth_client.py after adding a posture
 right_hand_postures = ['rh blank', 'rh hands together', 'rh other', 'rh beckon', 'rh claw down',
@@ -16,7 +18,9 @@ right_hand_postures = ['rh blank', 'rh hands together', 'rh other', 'rh beckon',
                        'rh open back', 'rh open down', 'rh open left', 'rh point down',
                        'rh point front', 'rh point left', 'rh stop', 'rh three back',
                        'rh three front', 'rh thumbs down', 'rh thumbs up', 'rh to face',
-                       'rh two back', 'rh two front', 'rh blind', 'rh grab cup']
+                       'rh two back', 'rh two front', 'rh new gesture 1', 'rh new gesture 2',
+                       'rh new gesture 3', 'rh new gesture 4', 'rh new gesture 5', 'rh learning',
+                       'rh learned', 'rh blind']
 
 left_arm_motions = ['la move right', 'la move left', 'la move up', 'la move down', 'la move back',
                     'la move front', 'la move right up', 'la move right down', 'la move right back',
