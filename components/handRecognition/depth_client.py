ec6794a417ac5362db673455f1d3e7bdeb102785
David White <david.white.679@gmail.com>
2019-03-14 18:08:30 -0600

    reapplying hand combination silencing excessive prints fixing another git-lfs-related issue

--- components/handRecognition/depth_client.py
+++ components/handRecognition/depth_client.py
@@ -39,18 +39,24 @@ def decode_content(raw_frame, offset):
     return (width, height, posx, posy, list(depth_data)), offset
 
 
-def decode_and_send_frame(frame, gestures, stream_id, fusion_socket, flip):
-    print('decoding frame')
-    timestamp, frame_type, width, height, posx, posy, depth_data = decode_frame(frame)
+def recv_process_reply(fusion_socket, kinect_socket, gestures, stream_id, name, flip):
+    try:
+        (timestamp, frame_type), (width, height, posx, posy, depth_data), (writer_data,) = decode.read_frame(
+            kinect_socket, decode_content)
+        #print("timestamp, frame_type", timestamp, frame_type)
+        #print("width, height, posx, posy", width, height, posx, posy)
+        #print("writer_data", writer_data)
+
+    except KeyboardInterrupt:
+        return False
 
     if posx == -1 and posy == -1:
-        probs = [0] * num_gestures + [1]
+        probs = [0.0] * num_gestures + [1.0]
         max_index = len(probs) - 1
-        print("can't find hand")
 
     else:
         hand_arr = np.array(depth_data, dtype=np.float32).reshape((height, width))
-        print(hand_arr.shape, posx, posy)
+        #print(hand_arr.shape, posx, posy)
         posz = hand_arr[int(posx), int(posy)]
         hand_arr -= posz
         hand_arr /= 150
@@ -58,27 +64,28 @@ def decode_and_send_frame(frame, gestures, stream_id, fusion_socket, flip):
         hand_arr = resize(hand_arr, (168, 168))
         hand_arr = hand_arr[20:-20, 20:-20]
         hand_arr = hand_arr.reshape((1, 128, 128, 1))
-        print('classifying frame')
         max_index, probs = hand_classfier.classify(hand_arr, flip)
 
-        probs = list(probs) + [0]
+        probs = list(probs) + [0.0]
 
-    print(i, timestamp, gestures[max_index], probs[max_index])
+    print(name, gestures[max_index], '{:.2}'.format(probs[max_index]), end='\t')
 
     pack_list = [stream_id, timestamp, max_index] + list(probs)
 
-    bytes = struct.pack("<iqi" + "f" * (num_gestures + 1), *pack_list)
+    msg = struct.pack("<iqi" + "f" * (num_gestures + 1), *pack_list)
 
     if fusion_socket is not None:
-        fusion_socket.send(bytes)
+        fusion_socket.send(msg)
+
+    return True
 
 
 # By default read 100 frames
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--kinect_host', '-k', help='Host name of the machine running Kinect Server')
-    parser.add_argument('--fusion-host', '-f', help='Host name of the machine running Kinect Server', default=None)
+    parser.add_argument('kinect_host', help='Host name of the machine running Kinect Server', default='localhost')
+    parser.add_argument('--fusion-host', help='Host name of the machine running Fusion Server', default='localhost')
 
     args = parser.parse_args()
 
@@ -99,22 +106,23 @@ if __name__ == '__main__':
 
     RH_fusion_socket = connect('fusion', args.fusion_host, "RH") if args.fusion_host is not None else None
     LH_fusion_socket = connect('fusion', args.fusion_host, "LH") if args.fusion_host is not None else None
-    print('connected to fusion and kinect')
+
     i = 0
     hands_list = []
-    while True:
-        try:
-            print('receiving frames')
-            RH_frame = recv_depth_frame(RH_kinect_socket)
-            LH_frame = recv_depth_frame(LH_kinect_socket)
-        except KeyboardInterrupt:
-           break
-        except:
-            continue
 
-        decode_and_send_frame(RH_frame, RH_gestures, RH_stream_id, RH_fusion_socket, flip=False)
-        decode_and_send_frame(LH_frame, LH_gestures, LH_stream_id, LH_fusion_socket, flip=True)
-        timer.wait(FPS=30)
+    start_time = time.time()
+    while True:
+        if not recv_process_reply(RH_fusion_socket, RH_kinect_socket, RH_gestures, RH_stream_id, 'RH', flip=False):
+            break
+        if not recv_process_reply(LH_fusion_socket, LH_kinect_socket, LH_gestures, LH_stream_id, 'LH', flip=True):
+            break
+
+        print()
+        i += 1
+
+        if i % 100 == 0:
+            print("=" * 80, "FPS", 100 / (time.time() - start_time))
+            start_time = time.time()
 
     RH_kinect_socket.close()
     LH_kinect_socket.close()
@@ -124,6 +132,4 @@ if __name__ == '__main__':
         LH_fusion_socket.close()
 
     sys.exit(0)
-
-
 
