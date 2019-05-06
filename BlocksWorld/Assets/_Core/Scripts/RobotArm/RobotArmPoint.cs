6374aee28f9c43535ee5eed3df9d7f4253d8538a
Joe Strout <joe@strout.net>
2019-05-06 06:46:34 -0700

    Finished the robot arm pointing behavior.

--- BlocksWorld/Assets/_Core/Scripts/RobotArm/RobotArmPoint.cs
+++ BlocksWorld/Assets/_Core/Scripts/RobotArm/RobotArmPoint.cs
@@ -11,7 +11,7 @@ using UnityEngine;
 
 public class RobotArmPoint : MonoBehaviour
 {
-	public RobotServo baseServo;
+	public RobotServo[] servos;
 	
 	public float debugAngle;
 		
@@ -19,13 +19,22 @@ public class RobotArmPoint : MonoBehaviour
 		if (DataStore.GetStringValue("me:intent:action") != "point") return;
 		Vector3 target = DataStore.GetVector3Value("me:intent:target");
 		
-		// Find the 2D angle normal to the Y axis.
+		// Find the 2D angle normal to the Y axis.  Point the base servo in that direction.
 		Vector3 pos = transform.position;
 		float ang = 90 - Mathf.Atan2(target.z - pos.z, target.x - pos.x) * Mathf.Rad2Deg;
 		debugAngle = ang;
-		baseServo.targetAngle = ang;
+		servos[0].targetAngle = ang;
 		
-		// That points the base.  ToDo: get the other joints to point the gripper
-		// directly at the target position (perhaps with IK).
+		// Put the arm joints in a standard "parked" position.
+		servos[1].target = 0.85f;
+		servos[2].target = 0.10f;
+		
+		// Now, point the gripper right at the target.
+		// Start by getting the target position relative to the wrist joint.
+		Vector3 localTarg = servos[2].transform.InverseTransformPoint(target)
+						  - servos[3].transform.localPosition;
+		// Then we can compute the proper angle for the last joint.
+		ang = Mathf.Atan2(localTarg.y, localTarg.x) * Mathf.Rad2Deg + 90;
+		servos[3].targetAngle = ang;
 	}
 }
