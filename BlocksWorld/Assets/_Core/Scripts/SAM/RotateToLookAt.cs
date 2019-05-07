62773f8377b1aaf9e5126bfa21713c1c914b8f7f
Joe Strout <joe@strout.net>
2019-05-06 22:20:13 -0700

    Added IK head-look module, applied to make SAM look where you point.

---
+++ BlocksWorld/Assets/_Core/Scripts/SAM/RotateToLookAt.cs
@@ -0,0 +1,37 @@
+﻿/*
+This simple script rotates the Transform it's attached to to point at a point
+obtained from the blackboard.  Used, for example, to make SAM look where the
+user is pointing.
+
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateToLookAt : MonoBehaviour
+{
+	public string targetKey = "user:pointPos";
+	public float speed = 720;
+	public Vector3 rotOffset = new Vector3(0, -90, -90);
+	
+	Quaternion startRot;
+	
+	protected void Start() {
+		startRot = transform.localRotation;
+	}
+
+	protected void Update() {
+		Quaternion targetRot;
+		Vector3 targetPos = DataStore.GetVector3Value(targetKey);
+		if (targetPos.sqrMagnitude < 0.01f) {
+			// no target set
+			targetRot = startRot;
+		} else {
+			// look towards the target
+			Vector3 dir = (targetPos - transform.position).normalized;
+			targetRot = Quaternion.LookRotation(dir) * Quaternion.Euler(rotOffset);
+		}
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, speed * Time.deltaTime);
+	}
+
+}
