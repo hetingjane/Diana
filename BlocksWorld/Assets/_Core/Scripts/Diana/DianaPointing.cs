4b780246ea9257dbbd5e50c83ad47cd37288a86d
Rahul Bangar <bangar1991@gmail.com>
2019-05-15 23:59:22 -0600

    Add pointing demo for Diana

---
+++ BlocksWorld/Assets/_Core/Scripts/Diana/DianaPointing.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DianaPointing : MonoBehaviour
+{
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        Debug.Assert(animator != null);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            animator.SetBool("grab", !animator.GetBool("grab"));
+        }
+
+        if (DataStore.GetStringValue("me:intent:action", "") == "point")
+        {
+            Vector3 target = DataStore.GetVector3Value("me:intent:target");
+            target += Vector3.up * 0.10f;
+
+            animator.SetFloat("x", target.x);
+            animator.SetFloat("z", target.z);
+        }
+    }
+}
