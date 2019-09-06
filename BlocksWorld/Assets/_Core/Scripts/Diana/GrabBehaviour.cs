517609c2f69cb7eeea2c6a8e890f23875eba3acd
Rahul Bangar <bangar1991@gmail.com>
2019-09-06 00:08:38 -0600

    Update DianaPointingCtrl controller to use cross reach animations

--- BlocksWorld/Assets/_Core/Scripts/Diana/GrabBehaviour.cs
+++ BlocksWorld/Assets/_Core/Scripts/Diana/GrabBehaviour.cs
@@ -1,57 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public enum Hand
+
+
+public class GrabBehaviour : StateMachineBehaviour
 {
-    LEFT,
-    RIGHT
-}
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+    }
 
-
-public class GrabBehaviour : StateMachineBehaviour {
-    public Hand hand;
-   
-
-    [HideInInspector]
-    public static GameObject target;
-
-    // Cache parameter ids for later use
-    private readonly int PositionWeightId = Animator.StringToHash("PositionWeight");
-    private readonly int RotationWeightId = Animator.StringToHash("RotationWeight");
-    private readonly int LookAtWeightId = Animator.StringToHash("LookAtWeight");
-    private bool grabbed = false;
-    
     // OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
     override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         base.OnStateIK(animator, stateInfo, layerIndex);
 
-        animator.SetLookAtWeight(animator.GetFloat(LookAtWeightId));
-        animator.SetIKPositionWeight(hand == Hand.LEFT ? AvatarIKGoal.LeftHand : AvatarIKGoal.RightHand, animator.GetFloat(PositionWeightId));
-        animator.SetIKRotationWeight(hand == Hand.LEFT ? AvatarIKGoal.LeftHand : AvatarIKGoal.RightHand, animator.GetFloat(RotationWeightId));
+        animator.SetIKPositionWeight(AvatarIKGoal.RightHand, animator.GetFloat("PositionWeight"));
+        animator.SetIKRotationWeight(AvatarIKGoal.RightHand, animator.GetFloat("RotationWeight"));
 
-        if (target != null)
-        {
-            animator.SetLookAtPosition(target.transform.position);
-            animator.SetIKPosition(hand == Hand.LEFT ? AvatarIKGoal.LeftHand : AvatarIKGoal.RightHand, target.transform.position);
-            animator.SetIKRotation(hand == Hand.LEFT ? AvatarIKGoal.LeftHand : AvatarIKGoal.RightHand, target.transform.rotation);
-        }
-    }
+        Vector3 target = DataStore.GetVector3Value("user:pointPos");
+        target += Vector3.up * 0.10f;
 
-    //public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    base.OnStateUpdate(animator, stateInfo, layerIndex);
-    //    if (target != null)
-    //    {
-    //        animator.SetFloat("x", target.transform.position.x);
-    //        animator.SetFloat("y", target.transform.position.z);
-    //    }
-    //}
-
-    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        base.OnStateEnter(animator, stateInfo, layerIndex);
-        grabbed = false;
+        animator.SetIKPosition(AvatarIKGoal.RightHand, target);
+        animator.SetIKRotation(AvatarIKGoal.RightHand, Quaternion.identity);
     }
 }
