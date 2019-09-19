593add26bffbf91b059315714ef8178be91238e7
Rahul Bangar <bangar1991@gmail.com>
2019-09-18 22:49:31 -0600

    Add documentation to DianaMotorControl

--- BlocksWorld/Assets/_Core/Scripts/Diana/DianaMotorControl.cs
+++ BlocksWorld/Assets/_Core/Scripts/Diana/DianaMotorControl.cs
@@ -1,35 +1,109 @@
 ﻿using System.Collections;
 using UnityEngine;
 
+/// <summary>
+/// This script makes a humanoid place the hand at a specified position.
+/// When <c>me:intent:handPosR</c> is changed, it can respond in different ways:
+/// - If Diana is not in grab animation, it starts the grab animation towards the intended hand position
+///   and sets the <c>me:rightArm:motion</c> key to "reaching"
+/// - If Diana completes the grab animation, the key <c>me:rightArm:motion</c> is set to "reached"
+/// - If <c>me:intent:handPosR</c> is set while "reaching" or having "reached", it starts manipulating the
+///   ongoing animation so that it ends up at new set value. During this operation, <c>me:rightArm:motion</c>
+///   is set to "moving"
+/// - If <c>me:intent:handPosR</c> is set to default value of <see cref="Vector3"/>, the ungrab animation starts.
+/// Reads:		me:intent:handPosR		The intended position of the hand
+/// Writes:		me:rightArm:motion		The motion that Diana is doing with right arm
+///				me:actual:handPosR		The actual position of the hand updated every frame
+/// </summary>
 [RequireComponent(typeof(Animator))]
 public class DianaMotorControl : MonoBehaviour
 {
-    private Animator animator;
-	
+	/// <summary>
+	/// Reference to the Animator
+	/// </summary>
+	private Animator animator;
+
+	#region Key values
+	/// <summary>
+	/// <see cref="DataStore.StringValue"/> equivalent of the string <c>"reaching"</c>.
+	/// Represents the state when the agent is in grab animation and the animation hasn't
+	/// finished yet.
+	/// </summary>
 	private readonly DataStore.StringValue reaching = new DataStore.StringValue("reaching");
+
+	/// <summary>
+	/// <see cref="DataStore.StringValue"/> equivalent of the string <c>"reached"</c>.
+	/// Represents teh state when the agent has completed the grab animation.
+	/// </summary>
 	private readonly DataStore.StringValue reached = new DataStore.StringValue("reached");
+
+	/// <summary>
+	/// <see cref="DataStore.StringValue"/> equivalent of the string <c>"moving"</c>.
+	/// Represents the state when the agent's hand position is being manually manipulated 
+	/// to be along a trajectory.
+	/// </summary>
 	private readonly DataStore.StringValue moving = new DataStore.StringValue("moving");
+
+	/// <summary>
+	/// <see cref="DataStore.StringValue"/> equivalent of the string <c>"unreaching"</c>.
+	/// Represents the state when the agent is in ungrab animation.
+	/// </summary>
 	private readonly DataStore.StringValue unreaching = new DataStore.StringValue("unreaching");
 
+	/// <summary>
+	/// <see cref="DataStore.StringValue"/> equivalent of the string <c>"idle"</c>.
+	/// Represents the state when the agent is in idle animation.
+	/// </summary>
 	private readonly DataStore.StringValue idle = new DataStore.StringValue("idle");
-	private readonly DataStore.StringValue waving = new DataStore.StringValue("waving");
 
+	/// <summary>
+	/// <see cref="DataStore.StringValue"/> equivalent of the string <c>"waving"</c>.
+	/// Represents the state when the agent is in wave animation.
+	/// </summary>
+	private readonly DataStore.StringValue waving = new DataStore.StringValue("waving");
+	#endregion
+
+	/// <summary>
+	/// The key to which this script writes on the blackboard. The key
+	/// describes Diana's animation state (manual or automatic).
+	/// </summary>
 	private const string meRightArmMotion = "me:rightArm:motion";
 
+	/// <summary>
+	/// Reference to the coroutine used to manipulate Diana's hand
+	/// position manually.
+	/// </summary>
 	private IEnumerator animatorLerpCoroutine;
 
+	/// <summary>
+	/// Movement speed when Diana's hand position is being manipulated manually.
+	/// Note that this does not affect the movement speed for genuine animations
+	/// like grab, ungrab, wave, etc.
+	/// </summary>
 	public float movementSpeed = .5f;
+
+	/// <summary>
+	/// Flag to mark if the hand is being moved manually or not.
+	/// </summary>
 	private bool isMoving = false;
 
-	// Start is called before the first frame update
+	/// <summary>
+	/// Establishes reference to the Animator component and
+	/// subscribes to <c>"me:intent:handPosR"</c> key.
+	/// </summary>
 	void Start()
-    {
-        animator = GetComponent<Animator>();
-        Debug.Assert(animator != null);
+	{
+		animator = GetComponent<Animator>();
+		Debug.Assert(animator != null);
 
 		DataStore.Subscribe("me:intent:handPosR", OnIntentHandPoseRChanged);
-    }
+	}
 
+	/// <summary>
+	/// The coroutine to move the hand manually to the specified position.
+	/// </summary>
+	/// <param name="intentXYZ">The intended position of the hand.</param>
+	/// <returns></returns>
 	IEnumerator LerpAnimatorXYZ(Vector3 intentXYZ)
 	{
 		Debug.Log("LerpAnimatorXYZ");
@@ -42,6 +116,7 @@ public class DianaMotorControl : MonoBehaviour
 
 		if (originalDistance > 0f)
 		{
+			// We start moving the hand now
 			isMoving = true;
 			float t = 0f;
 
@@ -64,6 +139,12 @@ public class DianaMotorControl : MonoBehaviour
 		animatorLerpCoroutine = null;
 	}
 
+	/// <summary>
+	/// Sets the animator x, y, z parameters to the specified ones.
+	/// </summary>
+	/// <param name="x">If specified, sets the value of <c>animator.x</c></param>
+	/// <param name="y">If specified, sets the value of <c>animator.y</c></param>
+	/// <param name="z">If specified, sets the value of <c>animator.z</c></param>
 	private void SetAnimatorXYZ(float x = float.NaN, float y = float.NaN, float z = float.NaN)
 	{
 		if (!float.IsNaN(x))
@@ -74,6 +155,11 @@ public class DianaMotorControl : MonoBehaviour
 			animator.SetFloat("z", z);
 	}
 
+	/// <summary>
+	/// The handler for whenever the <c>"me:intent:handPosR"</c> key changes.
+	/// </summary>
+	/// <param name="key"></param>
+	/// <param name="value"></param>
 	private void OnIntentHandPoseRChanged(string key, DataStore.IValue value)
 	{
 		Debug.Log("OnIntentHandPoseRChanged");
@@ -84,12 +170,14 @@ public class DianaMotorControl : MonoBehaviour
 			if (target != default)
 			{
 				// Prepare animator to start the grab motion towards the target
+				// Changes are instantaneous
 				animator.SetBool("grab", true);
 				SetAnimatorXYZ(x: target.x, y: target.y, z: target.z);
 			}
 		}
 		else
 		{
+			// If target is Vector3.default, we ungrab
 			if (target == default)
 			{
 				animator.SetBool("grab", false);
@@ -108,14 +196,16 @@ public class DianaMotorControl : MonoBehaviour
 		}
 	}
 
-	// Update is called once per frame
+	/// <summary>
+	/// Updates <c>"me:rightArm:motion"</c> when there's a change.
+	/// </summary>
 	void Update()
-    {
+	{
 		AnimatorStateInfo animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
 		var rightArmState = DataStore.GetValue(meRightArmMotion) as DataStore.StringValue;
 
 		if (animatorStateInfo.IsName("Grab"))
-        {
+		{
 			// Set to grabbing if animation hasn't finished
 			if (animatorStateInfo.normalizedTime < 1f && rightArmState != reaching)
 				DataStore.SetValue(meRightArmMotion, reaching, null, "DianaMotorControl");
@@ -134,7 +224,7 @@ public class DianaMotorControl : MonoBehaviour
 				}
 			}
 		}
-        else if (animatorStateInfo.IsName("Ungrab"))
+		else if (animatorStateInfo.IsName("Ungrab"))
 		{
 			// Set to grabbing if animation hasn't finished
 			if (rightArmState != unreaching)
@@ -160,9 +250,12 @@ public class DianaMotorControl : MonoBehaviour
 
 	private string state = "";
 
-    private void LateUpdate()
-    {
-        Vector3 actualPos = animator.GetBoneTransform(HumanBodyBones.RightHand).position;
-        DataStore.SetValue("me:actual:handPosR", new DataStore.Vector3Value(actualPos), null, "DianaMotorControl");
-    }
+	/// <summary>
+	/// Sets <c>"me:actual:handPosR"</c> in case a module depends on it.
+	/// </summary>
+	private void LateUpdate()
+	{
+		Vector3 actualPos = animator.GetBoneTransform(HumanBodyBones.RightHand).position;
+		DataStore.SetValue("me:actual:handPosR", new DataStore.Vector3Value(actualPos), null, "DianaMotorControl");
+	}
 }
