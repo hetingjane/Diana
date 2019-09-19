27d6c422db8613e7fed755d5f09f815a8c37f3f6
Rahul Bangar <bangar1991@gmail.com>
2019-09-18 22:12:20 -0600

    Add a test script for DianaMotorControl

---
+++ BlocksWorld/Assets/_Core/Scripts/Diana/TestDianaMotorControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDianaMotorControl : MonoBehaviour
+{
+	public Transform target;
+
+	public bool followTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		followTarget = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (target != null && followTarget)
+		{
+			DataStore.SetValue("me:intent:handPosR", new DataStore.Vector3Value(target.position), null, "");
+			followTarget = false;
+		}
+    }
+}
