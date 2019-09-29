0c0e7d50fc84aea7e46b4fe0a1c11f7d33dfaa31
Rahul Bangar <bangar1991@gmail.com>
2019-09-29 00:52:00 -0600

    Add state machine classes for intents

---
+++ BlocksWorld/Assets/_Core/Scripts/CogArch/StateMachines/RuleStateMachine.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System;
+
+public abstract class RuleStateMachine<T> where T: Enum
+{
+	private Dictionary<T, Dictionary<T, Rule>> transitions;
+
+
+	private T currentState;
+
+	public T CurrentState
+	{
+		get
+		{
+			return currentState;
+		}
+
+		private set
+		{
+			currentState = value;
+
+			if (transitions.TryGetValue(currentState, out var stateToRule))
+			{
+				foreach (var rule in stateToRule.Values)
+					rule.Reset();
+			}
+		}
+	}
+	
+
+	public RuleStateMachine()
+	{
+		transitions = new Dictionary<T, Dictionary<T, Rule>>();
+		Initialize();
+	}
+
+	public RuleStateMachine(T initialState): this()
+	{
+		CurrentState = initialState;
+	}
+
+	private bool IsSameState(T first, T second)
+	{
+		return EqualityComparer<T>.Default.Equals(first, second);
+	}
+
+	public void SetTransitionRule(T fromState, T toState, Rule rule)
+	{
+		if (IsSameState(fromState, toState))
+			throw new ArgumentException("'fromState' and 'toState' cannot be the same states");
+
+		if (rule == null)
+			throw new ArgumentNullException("'rule' argument cannot be null");
+
+		if (transitions.TryGetValue(fromState, out var transitionToRule))
+		{
+			transitionToRule[toState] = rule;
+		}
+		else
+		{
+			transitions[fromState] = new Dictionary<T, Rule>() {
+				{ toState, rule }
+			};
+		}
+	}
+
+	public bool Evaluate()
+	{
+		foreach(var pair in transitions[CurrentState])
+		{
+			var (toState, rule) = (pair.Key, pair.Value);
+			if (rule.Evaluate())
+			{
+				CurrentState = toState;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	protected abstract void Initialize();
+}
---
+++ BlocksWorld/Assets/_Core/Scripts/CogArch/StateMachines/Rules/Rule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Rule
+{
+	protected readonly Func<bool> func;
+
+	protected bool lastFuncEvaluation;
+
+	public Rule(Func<bool> func)
+	{
+		this.func = func;
+		lastFuncEvaluation = false;
+	}
+
+	public virtual bool Evaluate()
+	{
+		lastFuncEvaluation = func();
+		return lastFuncEvaluation;
+	}
+
+	public virtual void Reset()
+	{
+		lastFuncEvaluation = false;
+	}
+}
