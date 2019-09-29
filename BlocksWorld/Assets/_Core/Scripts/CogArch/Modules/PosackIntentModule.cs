5e71f724ea6f9b3f40f16d930930055544aebfd2
Rahul Bangar <bangar1991@gmail.com>
2019-09-29 01:13:29 -0600

    Add Posack intent module

---
+++ BlocksWorld/Assets/_Core/Scripts/CogArch/Modules/PosackIntentModule.cs
@@ -0,0 +1,27 @@
+﻿public class PosackIntentModule : ModuleBase
+{
+	private PosackStateMachine posackStateMachine;
+
+	protected override void Start()
+    {
+		base.Start();
+
+		posackStateMachine = new PosackStateMachine();
+	}
+
+	private void Update()
+	{
+		if (posackStateMachine.Evaluate())
+		{
+			switch (posackStateMachine.CurrentState)
+			{
+				case PosackState.PosackStart:
+					DataStore.SetStringValue("user:intent:posack", new DataStore.StringValue("posack start"), this, "thumbs up with either hand");
+					break;
+				case PosackState.PosackStop:
+					DataStore.SetStringValue("user:intent:posack", new DataStore.StringValue("posack stop"), this, "");
+					break;
+			}
+		}
+	}
+}
