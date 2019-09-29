bc7a886309d02c69879f48b075bb27c00c08c0de
Rahul Bangar <bangar1991@gmail.com>
2019-09-29 01:14:48 -0600

    Add Nevermind intent module

---
+++ BlocksWorld/Assets/_Core/Scripts/CogArch/Modules/NevermindIntentModule.cs
@@ -0,0 +1,27 @@
+﻿public class NevermindIntentModule : ModuleBase
+{
+	private NevermindStateMachine nevermindStateMachine;
+
+	protected override void Start()
+    {
+		base.Start();
+
+		nevermindStateMachine = new NevermindStateMachine();
+	}
+
+	private void Update()
+	{
+		if (nevermindStateMachine.Evaluate())
+		{
+			switch (nevermindStateMachine.CurrentState)
+			{
+				case NevermindState.NevermindStart:
+					DataStore.SetStringValue("user:intent:nevermind", new DataStore.StringValue("nevermind start"), this, "thumbs up with either hand");
+					break;
+				case NevermindState.NevermindStop:
+					DataStore.SetStringValue("user:intent:nevermind", new DataStore.StringValue("nevermind stop"), this, "");
+					break;
+			}
+		}
+	}
+}
