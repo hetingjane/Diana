37ead31180208cb8127ac36d826e91a622cfe82f
Rahul Bangar <bangar1991@gmail.com>
2019-09-29 01:12:39 -0600

    Add Nevermind state machine

---
+++ BlocksWorld/Assets/_Core/Scripts/CogArch/StateMachines/NevermindStateMachine.cs
@@ -0,0 +1,41 @@
+﻿public enum NevermindState
+{
+	NevermindStop,
+	NevermindStart
+}
+
+public class NevermindStateMachine : RuleStateMachine<NevermindState>
+{
+	protected override void Initialize()
+	{
+		SetTransitionRule(NevermindState.NevermindStop, NevermindState.NevermindStart, new TimedRule(() =>
+		{
+			bool userIsEngaged = DataStore.GetBoolValue("user:isEngaged");
+
+			if (userIsEngaged)
+			{
+				string leftHandGesture = DataStore.GetStringValue("user:hands:left");
+				string rightHandGesture = DataStore.GetStringValue("user:hands:right");
+
+				return leftHandGesture == "stop" || rightHandGesture == "stop";
+			}
+
+			return false;
+		}, 100));
+
+		SetTransitionRule(NevermindState.NevermindStart, NevermindState.NevermindStop, new TimedRule(() =>
+		{
+			bool userIsEngaged = DataStore.GetBoolValue("user:isEngaged");
+
+			if (!userIsEngaged)
+				return true;
+			else
+			{
+				string leftHandGesture = DataStore.GetStringValue("user:hands:left");
+				string rightHandGesture = DataStore.GetStringValue("user:hands:right");
+
+				return leftHandGesture != "stop" && rightHandGesture != "stop";
+			}
+		}, 100));
+	}
+}
