237461fea0f1ad856e63bd82e32485d0fd58bee2
Rahul Bangar <bangar1991@gmail.com>
2019-09-29 01:13:50 -0600

    Add Negack intent module

---
+++ BlocksWorld/Assets/_Core/Scripts/CogArch/Modules/NegackIntentModule.cs
@@ -0,0 +1,28 @@
+﻿
+public class NegackIntentModule : ModuleBase
+{
+	private NegackStateMachine negackStateMachine;
+
+	protected override void Start()
+    {
+		base.Start();
+
+		negackStateMachine = new NegackStateMachine();
+	}
+
+	private void Update()
+	{
+		if (negackStateMachine.Evaluate())
+		{
+			switch (negackStateMachine.CurrentState)
+			{
+				case NegackState.NegackStart:
+					DataStore.SetStringValue("user:intent:negack", new DataStore.StringValue("negack start"), this, "thumbs down with either hand");
+					break;
+				case NegackState.NegackStop:
+					DataStore.SetStringValue("user:intent:negack", new DataStore.StringValue("negack stop"), this, "");
+					break;
+			}
+		}
+	}
+}
