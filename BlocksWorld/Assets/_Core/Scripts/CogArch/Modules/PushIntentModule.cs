cd836b4cba5ba4972d2880d5a91c7790e69d7719
Rahul Bangar <bangar1991@gmail.com>
2019-10-01 01:16:26 -0600

    Change string keys user:intent:push{Left,Right} to bool keys user:intent:isPush{Left,Right}

--- BlocksWorld/Assets/_Core/Scripts/CogArch/Modules/PushIntentModule.cs
+++ BlocksWorld/Assets/_Core/Scripts/CogArch/Modules/PushIntentModule.cs
@@ -18,10 +18,10 @@
 			switch (pushRightStateMachine.CurrentState)
 			{
 				case PushRightState.PushRightStart:
-					DataStore.SetStringValue("user:intent:pushRight", new DataStore.StringValue("push right start"), this, "pushing right with left hand");
+					DataStore.SetValue("user:intent:isPushRight", DataStore.BoolValue.True, this, "pushing right with left hand");
 					break;
 				case PushRightState.PushRightStop:
-					DataStore.SetStringValue("user:intent:pushRight", new DataStore.StringValue("push right stop"), this, "stopped pushing right with left hand");
+					DataStore.SetValue("user:intent:isPushRight", DataStore.BoolValue.False, this, "stopped pushing right with left hand");
 					break;
 			}
 		}
@@ -31,10 +31,10 @@
 			switch (pushLeftStateMachine.CurrentState)
 			{
 				case PushLeftState.PushLeftStart:
-					DataStore.SetStringValue("user:intent:pushLeft", new DataStore.StringValue("push left start"), this, "pushing left with right hand");
+					DataStore.SetValue("user:intent:isPushLeft", DataStore.BoolValue.True, this, "pushing left with right hand");
 					break;
 				case PushLeftState.PushLeftStop:
-					DataStore.SetStringValue("user:intent:pushLeft", new DataStore.StringValue("push left stop"), this, "stopped pushing left with right hand");
+					DataStore.SetValue("user:intent:isPushLeft", DataStore.BoolValue.False, this, "stopped pushing left with right hand");
 					break;
 			}
 		}
