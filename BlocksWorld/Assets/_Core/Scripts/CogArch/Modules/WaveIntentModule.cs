2e0bc494f5117df87e1c00eadf2818102a68efd7
Rahul Bangar <bangar1991@gmail.com>
2019-10-01 00:57:13 -0600

    Add Wave state machine and intent module

---
+++ BlocksWorld/Assets/_Core/Scripts/CogArch/Modules/WaveIntentModule.cs
@@ -0,0 +1,27 @@
+﻿public class WaveIntentModule : ModuleBase
+{
+	private WaveStateMachine waveStateMachine;
+
+	protected override void Start()
+	{
+		base.Start();
+
+		waveStateMachine = new WaveStateMachine();
+	}
+
+	private void Update()
+	{
+		if (waveStateMachine.Evaluate())
+		{
+			switch (waveStateMachine.CurrentState)
+			{
+				case WaveState.WaveStart:
+					DataStore.SetValue("user:intent:isWaving", DataStore.BoolValue.True, this, "waving with either hand");
+					break;
+				case WaveState.WaveStop:
+					DataStore.SetValue("user:intent:isWaving", DataStore.BoolValue.False, this, "");
+					break;
+			}
+		}
+	}
+}
