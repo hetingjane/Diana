1dccde3467a58a67e74560cd91f19286125f6d51
nkrishnaswamy <nkrishna@brandeis.edu>
2019-10-01 15:17:49 -0400

    Add EventManagerTestModule GRASP and UNGRASP methods Add tags and update scene to interface with EventManagerTestModule

---
+++ BlocksWorld/Assets/_Core/Scripts/CogArch/Modules/EventManagerTestModule.cs
@@ -0,0 +1,37 @@
+﻿/*
+This script interfaces with the VoxSim event manager
+
+Reads:      ...
+Writes:     ...
+
+*/
+using UnityEngine;
+
+using VoxSimPlatform.Core;
+
+public class EventManagerTestModule : ModuleBase
+{
+    public EventManager eventManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GRASP(object[] args)
+    {
+        Debug.Log("Diana's World: I'm grasping!");
+    }
+
+    public void UNGRASP(object[] args)
+    {
+        Debug.Log("Diana's World: I'm ungrasping!");
+    }
+}
