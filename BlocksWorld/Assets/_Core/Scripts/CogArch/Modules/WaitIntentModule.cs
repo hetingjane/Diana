832df8e8f5830a77ec093a54532317700bd4ea6c
Rahul Bangar <bangar1991@gmail.com>
2019-10-07 11:04:27 -0600

    Add Wait state machine and intent module

---
+++ BlocksWorld/Assets/_Core/Scripts/CogArch/Modules/WaitIntentModule.cs
@@ -0,0 +1,27 @@
+﻿public class WaitIntentModule : ModuleBase
+{
+	private WaitStateMachine waitStateMachine;
+
+	protected override void Start()
+    {
+		base.Start();
+
+		waitStateMachine = new WaitStateMachine();
+	}
+
+	private void Update()
+	{
+		if (waitStateMachine.Evaluate())
+		{
+			switch (waitStateMachine.CurrentState)
+			{
+				case WaitState.WaitStart:
+					DataStore.SetValue("user:intent:isWait", DataStore.BoolValue.True, this, "count one with either hand");
+					break;
+				case WaitState.WaitStop:
+					DataStore.SetValue("user:intent:isWait", DataStore.BoolValue.False, this, "");
+					break;
+			}
+		}
+	}
+}
---
+++ BlocksWorld/Assets/_Core/Scripts/CogArch/StateMachines/WaitStateMachine.cs
@@ -0,0 +1,41 @@
+﻿public enum WaitState
+{
+	WaitStop,
+	WaitStart
+}
+
+public class WaitStateMachine : RuleStateMachine<WaitState>
+{
+	protected override void Initialize()
+	{
+		SetTransitionRule(WaitState.WaitStop, WaitState.WaitStart, new TimedRule(() =>
+		{
+			bool userIsEngaged = DataStore.GetBoolValue("user:isEngaged");
+
+			if (userIsEngaged)
+			{
+				string leftHandGesture = DataStore.GetStringValue("user:hands:left");
+				string rightHandGesture = DataStore.GetStringValue("user:hands:right");
+
+				return leftHandGesture == "one front" || rightHandGesture == "one front";
+			}
+
+			return false;
+		}, 300));
+
+		SetTransitionRule(WaitState.WaitStart, WaitState.WaitStop, new TimedRule(() =>
+		{
+			bool userIsEngaged = DataStore.GetBoolValue("user:isEngaged");
+
+			if (!userIsEngaged)
+				return true;
+			else
+			{
+				string leftHandGesture = DataStore.GetStringValue("user:hands:left");
+				string rightHandGesture = DataStore.GetStringValue("user:hands:right");
+
+				return leftHandGesture != "one front" && rightHandGesture != "one front";
+			}
+		}, 100));
+	}
+}
