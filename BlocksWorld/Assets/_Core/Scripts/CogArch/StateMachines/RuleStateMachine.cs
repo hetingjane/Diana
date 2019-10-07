115d93591eb75b6e05c9f6daf72fde641f4693c0
Rahul Bangar <bangar1991@gmail.com>
2019-10-07 13:41:50 -0600

    Add documentation to RuleStateMachine

--- BlocksWorld/Assets/_Core/Scripts/CogArch/StateMachines/RuleStateMachine.cs
+++ BlocksWorld/Assets/_Core/Scripts/CogArch/StateMachines/RuleStateMachine.cs
@@ -1,13 +1,50 @@
 ﻿using System.Collections.Generic;
 using System;
 
+/// <summary>
+/// Base class for a state machine that has a fixed number of states such
+/// that the transitions between states are decided by <see cref="Rule"/>s.
+/// </summary>
+/// <typeparam name="T">an enumeration of the states</typeparam>
+/// <example>
+/// <code>
+/// public enum MyStates
+/// {
+///		Start,
+///		Stop
+/// }
+/// 
+/// public class MyOwnStateMachine: RuleStateMachine{MyStates}
+/// {
+///		protected override void Initialize()
+///		{
+///			SetTransitionRule(MyStates.Start, MyStates.Stop, Rule.True);
+///			SetTransitionRule(MyStates.Stop, MyStates.Start, Rule.False);
+///		}
+/// }
+/// </code>
+/// </example>
 public abstract class RuleStateMachine<T> where T: Enum
 {
+	/// <summary>
+	/// A mapping from initial state to a nested mapping. The nested mapping maps
+	/// a target state to the rule that guards the transition to that state from
+	/// the initial state.
+	/// </summary>
 	private Dictionary<T, Dictionary<T, Rule>> transitions;
 
-
+	/// <summary>
+	/// The current state
+	/// </summary>
 	private T currentState;
 
+	/// <summary>
+	/// The current state.
+	/// <para>
+	/// Setting the property resets all the rules transitioning out from the new state.
+	/// Setting also invokes <see cref="OnStateChanged"/> event.
+	/// </para>
+	/// </summary>
 	public T CurrentState
 	{
 		get
@@ -31,18 +68,33 @@ public abstract class RuleStateMachine<T> where T: Enum
 		}
 	}
 
+	/// <summary>
+	/// Event arguments that accompany a <see cref="OnStateChanged"/> event
+	/// </summary>
+	/// <typeparam name="T">an enumeration of the states</typeparam>
 	public class StateChangedArgs<T>: EventArgs
 	{
+		/// <summary>
+		/// State from which the transition was made (previous state)
+		/// </summary>
 		public T FromState
 		{
 			get;
 		}
 
+		/// <summary>
+		/// State to which the transition was made (current state)
+		/// </summary>
 		public T ToState
 		{
 			get;
 		}
 
+		/// <summary>
+		/// Create a StateChangedArgs instance
+		/// </summary>
+		/// <param name="fromState">State from which the transition was made (previous state)</param>
+		/// <param name="toState">State to which the transition was made (current state)</param>
 		public StateChangedArgs(T fromState, T toState)
 		{
 			FromState = fromState;
@@ -50,13 +102,22 @@ public abstract class RuleStateMachine<T> where T: Enum
 		}
 	}
 
+	/// <summary>
+	/// Event that is triggered whenever the state is changed
+	/// </summary>
 	public event EventHandler<StateChangedArgs<T>> OnStateChanged;
 	
-
+	/// <summary>
+	/// Create a rule state machine with initial state set to the first value of the <see cref="T"/> enumeration
+	/// </summary>
 	public RuleStateMachine(): this(default)
 	{
 	}
 
+	/// <summary>
+	/// Create a rule state machine with initial state set to the given value of the<see cref="T"/> enumeration 
+	/// </summary>
+	/// <param name="initialState">The state in which the state machine is initially in</param>
 	public RuleStateMachine(T initialState)
 	{
 		transitions = new Dictionary<T, Dictionary<T, Rule>>();
@@ -65,11 +126,23 @@ public abstract class RuleStateMachine<T> where T: Enum
 		Initialize();
 	}
 
+	/// <summary>
+	/// Check if two enumeration values are equal. This is safe to use with generics.
+	/// </summary>
+	/// <param name="first">first enumeration value</param>
+	/// <param name="second">second enumeration value</param>
+	/// <returns><c>true</c> if the two enumeration values are equal, <c>false</c> otherwise</returns>
 	private bool IsSameState(T first, T second)
 	{
 		return EqualityComparer<T>.Default.Equals(first, second);
 	}
 
+	/// <summary>
+	/// Sets a transition rule from one state to another using a <see cref="Rule"/>
+	/// </summary>
+	/// <param name="fromState">the state from which the transition will be made</param>
+	/// <param name="toState">the state to which the transition will be made</param>
+	/// <param name="rule">the rule that must be true for the transition to be made</param>
 	public void SetTransitionRule(T fromState, T toState, Rule rule)
 	{
 		if (IsSameState(fromState, toState))
@@ -90,6 +163,11 @@ public abstract class RuleStateMachine<T> where T: Enum
 		}
 	}
 
+	/// <summary>
+	/// Evaluates all the rules transitioning out of the current state.
+	/// If a rule evaluates to <c>true</c>, the transition is made to the new state.
+	/// </summary>
+	/// <returns><c>true</c> if the transition was made, <c>false</c> otherwise</returns>
 	public bool Evaluate()
 	{
 		foreach(var pair in transitions[CurrentState])
@@ -104,5 +182,10 @@ public abstract class RuleStateMachine<T> where T: Enum
 		return false;
 	}
 
+	/// <summary>
+	/// Override this in child classes to set up transitions using <see cref="SetTransitionRule(T, T, Rule)"/>
+	/// and to attach event handlers to <see cref="OnStateChanged"/> event. This method is called right after 
+	/// the constructor is called.
+	/// </summary>
 	protected abstract void Initialize();
 }
