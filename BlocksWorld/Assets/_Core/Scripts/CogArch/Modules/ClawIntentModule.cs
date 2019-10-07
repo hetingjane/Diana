1003edfc7465fdd7b2f6396194edc7b81b8c7397
Rahul Bangar <bangar1991@gmail.com>
2019-10-07 11:11:56 -0600

    Add Claw state machine and intent module

---
+++ BlocksWorld/Assets/_Core/Scripts/CogArch/Modules/ClawIntentModule.cs
@@ -0,0 +1,27 @@
+﻿public class ClawIntentModule : ModuleBase
+{
+	private ClawStateMachine clawStateMachine;
+
+	protected override void Start()
+    {
+		base.Start();
+
+		clawStateMachine = new ClawStateMachine();
+	}
+
+	private void Update()
+	{
+		if (clawStateMachine.Evaluate())
+		{
+			switch (clawStateMachine.CurrentState)
+			{
+				case ClawState.ClawStart:
+					DataStore.SetValue("user:intent:isClaw", DataStore.BoolValue.True, this, "claw down with either hand");
+					break;
+				case ClawState.ClawStop:
+					DataStore.SetValue("user:intent:isClaw", DataStore.BoolValue.False, this, "");
+					break;
+			}
+		}
+	}
+}
