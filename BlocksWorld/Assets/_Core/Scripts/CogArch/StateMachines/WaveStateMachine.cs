805a5508be0031c367ffa669f4f335c4a0252ac8
David White <david.white.679@gmail.com>
2019-10-08 14:01:43 -0600

    revise wave state machine: wave motions are now detected in the state machine (C#) itself

--- BlocksWorld/Assets/_Core/Scripts/CogArch/StateMachines/WaveStateMachine.cs
+++ BlocksWorld/Assets/_Core/Scripts/CogArch/StateMachines/WaveStateMachine.cs
@@ -1,4 +1,7 @@
-﻿public enum WaveState
+﻿using System;
+using UnityEngine;
+
+public enum WaveState
 {
 	WaveStart,
 	WaveStop
@@ -6,25 +9,70 @@
 
 public class WaveStateMachine : RuleStateMachine<WaveState>
 {
-	private bool IsUserWaving()
-	{
-		string rightArmMotion = DataStore.GetStringValue("user:arms:right");
-		string leftArmMotion = DataStore.GetStringValue("user:arms:left");
-		return rightArmMotion == "ra wave" || leftArmMotion == "la wave";
-	}
+    WaveHelper leftArmHelper = new WaveHelper(1.0, "user:joint:ElbowLeft", "user:joint:WristLeft");
+    WaveHelper rightArmHelper = new WaveHelper(1.0, "user:joint:ElbowRight", "user:joint:WristRight");
 
-	protected override void Initialize()
-	{
-		SetTransitionRule(WaveState.WaveStop, WaveState.WaveStart, new TimedRule(() =>
-		{
-			bool userIsEngaged = DataStore.GetBoolValue("user:isEngaged");
-			return userIsEngaged && IsUserWaving();
-		}, 300));
+    private bool isWaving()
+    {
+        bool userIsEngaged = DataStore.GetBoolValue("user:isEngaged");
+        return userIsEngaged && (leftArmHelper.IsArmWaving() || rightArmHelper.IsArmWaving());
+    }
 
-		SetTransitionRule(WaveState.WaveStart, WaveState.WaveStop, new TimedRule(() =>
-		{
-			bool userIsEngaged = DataStore.GetBoolValue("user:isEngaged");
-			return !userIsEngaged || !IsUserWaving();
-		}, 200));
-	}
+    protected override void Initialize()
+    {
+        SetTransitionRule(WaveState.WaveStop, WaveState.WaveStart, new Rule(() => isWaving()));
+        SetTransitionRule(WaveState.WaveStart, WaveState.WaveStop, new Rule(() => !isWaving()));
+    }
+}
+
+class WaveHelper
+{
+    private string elbowKey;
+    private string wristKey;
+    private bool wristWasLeftOfElbow = false;
+    private bool wristWasRightOfElbow = false;
+    private DateTime leftStart;
+    private DateTime rightStart;
+    private double secondsThreshold;
+
+    public WaveHelper(double secondsThreshold, string elbowKey, string wristKey)
+    {
+        this.secondsThreshold = secondsThreshold;
+        this.leftStart = DateTime.Now;
+        this.rightStart = DateTime.Now;
+        this.elbowKey = elbowKey;
+        this.wristKey = wristKey;
+    }
+
+    public bool IsArmWaving()
+    {
+        Vector3 wristPos = DataStore.GetVector3Value(wristKey);
+        Vector3 elbowPos = DataStore.GetVector3Value(elbowKey);
+
+        bool wristAboveElbow = wristPos.y > elbowPos.y;
+        bool wristLeftOfElbow = wristPos.x < elbowPos.x && wristAboveElbow;
+        bool wristRightOfElbow = wristPos.x > elbowPos.x && wristAboveElbow;
+
+        if (wristLeftOfElbow)
+        {
+            wristWasLeftOfElbow = true;
+            leftStart = DateTime.Now;
+        }
+        else if ((DateTime.Now - leftStart).TotalSeconds > secondsThreshold)
+        {
+            wristWasLeftOfElbow = false;
+        }
+
+        if (wristRightOfElbow)
+        {
+            wristWasRightOfElbow = true;
+            rightStart = DateTime.Now;
+        }
+        else if ((DateTime.Now - rightStart).TotalSeconds > secondsThreshold)
+        {
+            wristWasRightOfElbow = false;
+        }
+
+        return wristAboveElbow && wristWasRightOfElbow && wristWasLeftOfElbow;
+    }
 }
