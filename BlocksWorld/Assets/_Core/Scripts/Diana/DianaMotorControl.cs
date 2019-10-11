ee6f24997a47513f3f5728281d9c18950effb15a
Rahul Bangar <bangar1991@gmail.com>
2019-10-11 17:10:26 -0600

    Rework DianaMotorControl to allow controlling wave animation
    This will allow DianaMotorControl to control more animations as added.
    The main change that allows this is that DianaMotorControl now responds
    to me:intent:motion:rightArm for actually starting the animations. This
    way me:intent:handPosR only serves to change animator target position
    when the agent is not reaching, and to move the hand in
    linear motion when the agent is reaching or has reached the target.

--- BlocksWorld/Assets/_Core/Scripts/Diana/DianaMotorControl.cs
+++ BlocksWorld/Assets/_Core/Scripts/Diana/DianaMotorControl.cs
@@ -1,20 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 /// <summary>
-/// This script makes a humanoid place the hand at a specified position.
-/// When <c>me:intent:handPosR</c> is changed, it can respond in different ways:
-/// - If Diana is not in grab animation, it starts the grab animation towards the intended hand position
-///   and sets the <c>me:rightArm:motion</c> key to "reaching"
-/// - If Diana completes the grab animation, the key <c>me:rightArm:motion</c> is set to "reached"
-/// - If <c>me:intent:handPosR</c> is set while "reaching" or having "reached", it starts manipulating the
-///   ongoing animation so that it ends up at new set value. During this operation, <c>me:rightArm:motion</c>
-///   is set to "moving"
-/// - If <c>me:intent:handPosR</c> is set to default value of <see cref="Vector3"/>, the ungrab animation starts
-///   and <c>me:rightArm:motion</c> is set to "unreaching"
-/// Reads:		me:intent:handPosR		The intended position of the hand
-/// Writes:		me:rightArm:motion		The motion that Diana is doing with right arm
-///				me:actual:handPosR		The actual position of the hand updated every frame
+/// This script controls a humanoid's right arm motions like waving, grabbing, and other gestures.
+/// 
+/// <para>
+/// When <c>me:intent:motion:rightArm</c> is changed, it can respond in different ways depending on what value is set:
+/// 
+/// <list type="bullet">
+/// <item>
+/// <term>"reach"</term>			<description>If Diana is not in a grab animation, it starts the grab animation 
+///												 towards the target specified by <c>me:intent:handPosR</c> as long as it's not 
+///												 <see cref="default(Vector3)"/> and sets <c>me:actual:motion:rightArm</c> to 
+///												 <c>"reaching"</c></description>. When the grab animation completes, it sets
+///												 <c>me:actual:motion:rightArm</c> to <c>"reached"</c>
+///	</item>
+///	
+/// <item>
+/// <term>"unreach"</term>			<description>If Diana is <c>me:actual:motion:rightArm</c> is <c>"reaching"</c>, <c>"reached"</c>
+///												 or <c>"moving"</c>, then this will start the ungrab animation</description>
+///												 
+/// </item>
+/// 
+/// <item>
+/// <term>"wave"</term>				<description>Starts the wave animation</description>
+/// </item>
+/// 
+/// </list>
+/// </para>
+///
+/// <para>
+/// When <c>me:intent:handPosR</c> is changed when <c>me:actual:motion:rightArm</c> is <c>"reaching"</c> or <c>"reached"</c>,
+/// it starts manipulating the ongoing animation so that it ends up at new set value. During this operation, <c>me:actual:motion:rightArm</c>
+/// is set to "moving".
+/// </para>
+/// 
+/// <para>
+/// Reads:		<c>me:intent:handPosR</c>				The intended position of the hand
+///				<c>me:intent:motion:rightArm</c>		The intended motion with right arm. Possible values are "reach", "unreach", "wave"
+/// Writes:		<c>me:actual:handPosR</c>				The actual position of the hand updated every frame	
+///				<c>me:actual:motion:rightArm</c>		The motion that Diana is doing with right arm. Possible values
+///														are "idle", "reaching", "reached", "moving", "waving", "unreaching"
+///				
+/// </para>
 /// </summary>
 [RequireComponent(typeof(Animator))]
 public class DianaMotorControl : MonoBehaviour
@@ -68,7 +97,7 @@ public class DianaMotorControl : MonoBehaviour
 	/// The key to which this script writes on the blackboard. The key
 	/// describes Diana's animation state (manual or automatic).
 	/// </summary>
-	private const string meRightArmMotion = "me:rightArm:motion";
+	private const string actualMotionRightArm = "me:actual:motion:rightArm";
 
 	/// <summary>
 	/// Reference to the coroutine used to manipulate Diana's hand
@@ -100,6 +129,66 @@ public class DianaMotorControl : MonoBehaviour
 		Debug.Assert(animator != null);
 
 		DataStore.Subscribe("me:intent:handPosR", OnIntentHandPoseRChanged);
+		DataStore.Subscribe("me:intent:motion:rightArm", OnIntentMotionRightArmChanged);
+	}
+
+	/// <summary>
+	/// The handler for whenever the <c>"me:intent:handPosR"</c> key changes.
+	/// </summary>
+	/// <param name="key"></param>
+	/// <param name="value"></param>
+	private void OnIntentHandPoseRChanged(string key, DataStore.IValue value)
+	{
+		//Debug.Log("OnIntentHandPoseRChanged");
+		Vector3 target = (value as DataStore.Vector3Value).val;
+
+		// If target is not Vector3.default, we move
+		if (target != default)
+		{
+			// If already grabbing/grabbed
+			if (animator.GetBool("grab"))
+			{
+				// Changes should be over time
+				if (animatorLerpCoroutine != null)
+				{
+					StopCoroutine(animatorLerpCoroutine);
+				}
+
+				animatorLerpCoroutine = LerpAnimatorXYZ(target);
+				StartCoroutine(animatorLerpCoroutine);
+			}
+			else
+			{
+				// Set animator position parameters in anticipation of future writes to
+				// me:intent:motion:rightArm
+				SetAnimatorXYZ(target.x, target.y, target.z);
+			}
+		}
+	}
+
+	private void OnIntentMotionRightArmChanged(string key, DataStore.IValue value)
+	{
+		string motion = (value as DataStore.StringValue).val;
+
+		if (motion == "reach")
+		{
+			if (!animator.GetBool("grab"))
+			{
+				Vector3 target = DataStore.GetVector3Value("me:intent:handPosR");
+				// Prepare animator to start the grab motion towards the target
+				// Changes are instantaneous
+
+				if (target != default)
+				{
+					SetAnimatorXYZ(x: target.x, y: target.y, z: target.z);
+					animator.SetBool("grab", true);
+				}
+			}
+		}
+		else if (motion == "unreach")
+			animator.SetBool("grab", false);
+		else if (motion == "wave")
+			animator.SetBool("wave", true);
 	}
 
 	/// <summary>
@@ -159,92 +248,53 @@ public class DianaMotorControl : MonoBehaviour
 	}
 
 	/// <summary>
-	/// The handler for whenever the <c>"me:intent:handPosR"</c> key changes.
-	/// </summary>
-	/// <param name="key"></param>
-	/// <param name="value"></param>
-	private void OnIntentHandPoseRChanged(string key, DataStore.IValue value)
-	{
-		//Debug.Log("OnIntentHandPoseRChanged");
-		Vector3 target = (value as DataStore.Vector3Value).val;
-
-		if (!animator.GetBool("grab"))
-		{
-			if (target != default)
-			{
-				// Prepare animator to start the grab motion towards the target
-				// Changes are instantaneous
-				animator.SetBool("grab", true);
-				SetAnimatorXYZ(x: target.x, y: target.y, z: target.z);
-			}
-		}
-		else
-		{
-			// If target is Vector3.default, we ungrab
-			if (target == default)
-			{
-				animator.SetBool("grab", false);
-			}
-			else
-			{
-				// Changes should be over time
-				if (animatorLerpCoroutine != null)
-				{
-					StopCoroutine(animatorLerpCoroutine);
-				}
-
-				animatorLerpCoroutine = LerpAnimatorXYZ(target);
-				StartCoroutine(animatorLerpCoroutine);
-			}
-		}
-	}
-
-	/// <summary>
-	/// Updates <c>"me:rightArm:motion"</c> when there's a change.
+	/// Updates <c>"me:actual:motion:rightArm"</c> when there's a change.
 	/// </summary>
 	void Update()
 	{
 		AnimatorStateInfo animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
-		var rightArmState = DataStore.GetValue(meRightArmMotion) as DataStore.StringValue;
+		var rightArmState = DataStore.GetValue(actualMotionRightArm) as DataStore.StringValue;
 
 		if (animatorStateInfo.IsName("Grab"))
 		{
-			// Set to grabbing if animation hasn't finished
+			// Set to reaching if animation hasn't finished
 			if (animatorStateInfo.normalizedTime < 1f && rightArmState != reaching)
-				DataStore.SetValue(meRightArmMotion, reaching, null, "DianaMotorControl");
-			// Set to grabbed if animation has finished
+				DataStore.SetValue(actualMotionRightArm, reaching, null, "DianaMotorControl");
+			// Set to reached if animation has finished
 			else if (animatorStateInfo.normalizedTime > 1f)
 			{
+				// unless it is moving
 				if (isMoving)
 				{
+					// in which case set it to moving if it's not already set
 					if (rightArmState != moving)
-						DataStore.SetValue(meRightArmMotion, moving, null, "DianaMotorControl");
+						DataStore.SetValue(actualMotionRightArm, moving, null, "DianaMotorControl");
 				}
 				else
 				{
 					if (rightArmState != reached)
-						DataStore.SetValue(meRightArmMotion, reached, null, "DianaMotorControl");
+						DataStore.SetValue(actualMotionRightArm, reached, null, "DianaMotorControl");
 				}
 			}
 		}
 		else if (animatorStateInfo.IsName("Ungrab"))
 		{
-			// Set to grabbing if animation hasn't finished
+			// Set to unreaching if animation hasn't finished
 			if (rightArmState != unreaching)
-				DataStore.SetValue(meRightArmMotion, unreaching, null, "DianaMotorControl");
+				DataStore.SetValue(actualMotionRightArm, unreaching, null, "DianaMotorControl");
 		}
 		else if (animatorStateInfo.IsName("Idle"))
 		{
 			if (rightArmState != idle)
-				DataStore.SetValue(meRightArmMotion, idle, null, "DianaMotorControl");
+				DataStore.SetValue(actualMotionRightArm, idle, null, "DianaMotorControl");
 		}
 		else if (animatorStateInfo.IsName("Wave"))
 		{
 			if (rightArmState != waving)
-				DataStore.SetValue(meRightArmMotion, waving, null, "DianaMotorControl");
+				DataStore.SetValue(actualMotionRightArm, waving, null, "DianaMotorControl");
 		}
 
-		var curState = DataStore.GetStringValue(meRightArmMotion, defaultValue: string.Empty);
+		var curState = DataStore.GetStringValue(actualMotionRightArm, defaultValue: string.Empty);
 		if (rightArmState == null || curState != rightArmState.val)
 		{
 			Debug.Log($"State: {curState}");
