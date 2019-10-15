3f055cf52f593d12d6bc90713008cb5a03255b89
Rahul Bangar <bangar1991@gmail.com>
2019-10-14 23:15:09 -0600

    Add Servo intent module and state machine scripts

---
+++ BlocksWorld/Assets/_Core/Scripts/CogArch/StateMachines/ServoLeftStateMachine.cs
@@ -0,0 +1,38 @@
+﻿public enum ServoLeftState
+{
+	ServoLeftStop,
+	ServoLeftStart
+}
+
+public class ServoLeftStateMachine : RuleStateMachine<ServoLeftState>
+{
+	public ServoLeftStateMachine()
+	{
+		SetTransitionRule(ServoLeftState.ServoLeftStop, ServoLeftState.ServoLeftStart, new TimedRule(() =>
+		{
+			bool userIsEngaged = DataStore.GetBoolValue("user:isEngaged");
+
+			if (userIsEngaged)
+			{
+				string rightHandGesture = DataStore.GetStringValue("user:hands:right");
+				string rightArmServo = DataStore.GetStringValue("user:armMotion:right");
+				return (rightHandGesture == "open left" || rightHandGesture == "closed left") && rightArmServo == "servo";
+			}
+			return false;
+		}, 300));
+
+		SetTransitionRule(ServoLeftState.ServoLeftStart, ServoLeftState.ServoLeftStop, new TimedRule(() =>
+		{
+			bool userIsEngaged = DataStore.GetBoolValue("user:isEngaged");
+
+			if (!userIsEngaged)
+				return true;
+			else
+			{
+				string rightHandGesture = DataStore.GetStringValue("user:hands:right");
+				string rightArmServo = DataStore.GetStringValue("user:armMotion:right");
+				return (rightHandGesture != "open left" && rightHandGesture != "closed left") || rightArmServo != "servo";
+			}
+		}, 100));
+	}
+}
