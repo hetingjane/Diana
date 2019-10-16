7e5bfc6e1b568cbc196cb036c7e382636336a723
Rahul Bangar <bangar1991@gmail.com>
2019-10-16 15:29:28 -0600

    Add Grasp module (replacement for GrabPlaceModule)

---
+++ BlocksWorld/Assets/_Core/Scripts/CogArch/Modules/GraspModule.cs
@@ -0,0 +1,209 @@
+﻿using VoxSimPlatform.CogPhysics;
+using VoxSimPlatform.Global;
+using VoxSimPlatform.Vox;
+using UnityEngine;
+
+public enum GraspState
+{
+	/// <summary>
+	/// Hand is in relaxed position
+	/// </summary>
+	Idle,
+	/// <summary>
+	/// Hand begins to be stretched out to reach the object
+	/// </summary>
+	Grasping,
+	/// <summary>
+	/// Hand is stationary with the object held
+	/// </summary>
+	Holding,
+	/// <summary>
+	/// Hand begins to retreat towards relaxed position
+	/// </summary>
+	Ungrasping
+}
+
+public class GraspModule : ModuleBase
+{
+	/// <summary>
+	/// Reference to the animator attached to the humanoid character
+	/// </summary>
+	[Tooltip("Drag drop the character model here")]
+	public Animator animator;
+
+	/// <summary>
+	/// The object to reach
+	/// </summary>
+	private Voxeme target;
+
+	/// <summary>
+	/// Placeholder for directing hand movements
+	/// </summary>
+	private Vector3 movePosition;
+
+	/// <summary>
+	/// Object being held currently
+	/// </summary>
+	private Voxeme held;
+
+	/// <summary>
+	/// Reference to the effector bone to be used for manipulating objects
+	/// </summary>
+	private Transform hand;
+
+	/// <summary>
+	/// Current state
+	/// </summary>
+	private GraspState currentState;
+
+	// Start is called before the first frame update
+	protected override void Start()
+    {
+		base.Start();
+		Debug.Assert(animator != null);
+		hand = animator.GetBoneTransform(HumanBodyBones.RightHand);
+		Debug.Assert(hand != null);
+		currentState = GraspState.Idle;
+	}
+
+	private const string graspAction = "grasp";
+	private const string moveAction = "move";
+	private const string ungraspAction = "ungrasp";
+
+	// Update is called once per frame
+	void Update()
+    {
+		string action = DataStore.GetStringValue("me:intent:action");
+		string rightArmMotion = DataStore.GetStringValue("me:actual:motion:rightArm");
+		
+		switch (currentState)
+		{
+			case GraspState.Idle:
+				if (action == graspAction)
+				{
+					// Try to resolve the target by name
+					string targetName = DataStore.GetStringValue("me:intent:targetName");
+					// If the target is a named object get its Voxeme component
+					var curTarget = string.IsNullOrEmpty(targetName) ? null : GameObject.Find(targetName).GetComponent<Voxeme>();
+
+					Vector3 curMovePosition = DataStore.GetVector3Value("me:intent:target");
+
+					if (curTarget != null && curMovePosition != default)
+					{
+						target = curTarget;
+						movePosition = curMovePosition;
+
+						SetValue("me:intent:handPosR", movePosition, currentState.ToString());
+						SetValue("me:intent:motion:rightArm", new DataStore.StringValue("reach"), "");
+						SetValue("me:intent:action:isComplete", false, "");
+						// We begin grasping
+						currentState = GraspState.Grasping;
+					}
+				}
+				break;
+			case GraspState.Grasping:
+				if (rightArmMotion == "reached")
+				{
+					held = target;
+					SetValue("me:holding", held.name, $"Holding {held.name}");
+
+					SetValue("me:intent:action:isComplete", true, "");
+					currentState = GraspState.Holding;
+				}
+				else if (action == ungraspAction)
+				{
+					SetValue("me:intent:motion:rightArm", new DataStore.StringValue("unreach"), "");
+					SetValue("me:intent:action:isComplete", false, "");
+					currentState = GraspState.Ungrasping;
+				}
+				else if (action == graspAction)
+				{
+					// Try to resolve the target by name
+					string targetName = DataStore.GetStringValue("me:intent:targetName");
+					// If the target is a named object get its Voxeme component
+					var curTarget = string.IsNullOrEmpty(targetName) ? null : GameObject.Find(targetName).GetComponent<Voxeme>();
+
+					Vector3 curMovePosition = DataStore.GetVector3Value("me:intent:target");
+
+					if (curTarget != target && curMovePosition != default)
+					{
+						target = curTarget;
+						movePosition = curMovePosition;
+
+						SetValue("me:intent:handPosR", movePosition, currentState.ToString());
+					}
+				}
+				break;
+			case GraspState.Holding:
+				if (action == ungraspAction)
+				{
+					held = null;
+					SetValue("me:holding", "", $"Holding nothing");
+
+					target = null;
+					movePosition = default;
+
+					SetValue("me:intent:motion:rightArm", new DataStore.StringValue("unreach"), "");
+					SetValue("me:intent:action:isComplete", false, "");
+					currentState = GraspState.Ungrasping;
+				}
+				else if (action == graspAction)
+				{
+					// Try to resolve the target by name
+					string targetName = DataStore.GetStringValue("me:intent:targetName");
+
+					// If the target is a named object get its Voxeme component
+					var curTarget = string.IsNullOrEmpty(targetName) ? null : GameObject.Find(targetName).GetComponent<Voxeme>();
+
+					Vector3 curMovePosition = DataStore.GetVector3Value("me:intent:target");
+
+					if (curTarget != null && curTarget != target)
+					{
+						target = curTarget;
+						movePosition = curMovePosition;
+
+						SetValue("me:intent:handPosR", movePosition, currentState.ToString());
+						SetValue("me:intent:action:isComplete", false, "");
+
+						currentState = GraspState.Grasping;
+					}
+				}
+				else if (action == moveAction)
+				{
+					Vector3 curMovePosition = DataStore.GetVector3Value("me:intent:target");
+					movePosition = curMovePosition;
+					SetValue("me:intent:handPosR", movePosition, currentState.ToString());
+					SetValue("me:intent:action:isComplete", false, "");
+				}
+				break;
+			case GraspState.Ungrasping:
+				if (rightArmMotion == "idle")
+				{
+					SetValue("me:intent:action:isComplete", true, "");
+					currentState = GraspState.Idle;
+				}
+				else if (action == "grasp")
+				{
+					// Try to resolve the target by name
+					string targetName = DataStore.GetStringValue("me:intent:targetName");
+					// If the target is a named object get its Voxeme component
+					var curTarget = string.IsNullOrEmpty(targetName) ? null : GameObject.Find(targetName).GetComponent<Voxeme>();
+
+					Vector3 curMovePosition = DataStore.GetVector3Value("me:intent:target");
+
+					if (curTarget != null && curMovePosition != default)
+					{
+						target = curTarget;
+						movePosition = curMovePosition;
+
+						SetValue("me:intent:handPosR", movePosition, currentState.ToString());
+						SetValue("me:intent:motion:rightArm", new DataStore.StringValue("reach"), "");
+						SetValue("me:intent:action:isComplete", false, "");
+						// We begin grasping
+						currentState = GraspState.Grasping;
+					}
+				}
+				break;
+		}
+	}
+}
---
+++ BlocksWorld/Assets/_Core/Scripts/TestGraspModule.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VoxSimPlatform.CogPhysics;
+using VoxSimPlatform.Global;
+using VoxSimPlatform.Vox;
+
+public class TestGraspModule : MonoBehaviour
+{
+	public Transform manipulableObjectsRoot;
+
+	public bool doGraspRandomObject;
+	public bool doMoveRandomLocation;
+	public bool doUngrasp;
+
+	/// <summary>
+	/// Offset of the block's position (geometrical center in world coordinates) w.r.t hand bone.
+	/// Hand bone position + hold offset = block position
+	/// (Block) Position - hold offset = Hand bone position 
+	/// </summary>
+	private readonly Vector3 holdOffset = new Vector3(0f, -.08f, .04f);
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		doGraspRandomObject = false;
+		doMoveRandomLocation = false;
+		doUngrasp = false;
+
+		manipulableObjects = new GameObject[manipulableObjectsRoot.childCount];
+		for (int i=0; i<manipulableObjects.Length; i++)
+		{
+			manipulableObjects[i] = manipulableObjectsRoot.GetChild(i).gameObject;
+		}
+
+		targetBlock = GetRandomObject();
+    }
+
+	private Vector3 GetRandomLocation()
+	{
+		var x = Random.Range(-.45f, .45f);
+		var y = Random.Range(1.1f, 2f);
+		var z = Random.Range(-.25f, .25f);
+		return new Vector3(x, y, z);
+	}
+
+	private GameObject[] manipulableObjects;
+
+	private GameObject targetBlock;
+
+	private GameObject GetRandomObject()
+	{
+		return manipulableObjects[Random.Range(0, manipulableObjects.Length - 1)];
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doGraspRandomObject)
+		{
+			targetBlock = GetRandomObject();
+			DataStore.SetValue("me:intent:action", new DataStore.StringValue("grasp"), null, "");
+			DataStore.SetValue("me:intent:targetName", new DataStore.StringValue(targetBlock.name), null, "");
+			// Either by name or location, we successfully resolved the target object
+			// Get bounds of the Voxeme geometry
+			var bounds = GlobalHelper.GetObjectWorldSize(targetBlock.gameObject);
+
+			// The default set down position is the same as the initial position of the block
+			// in case the user backs out of the action
+			var setDownPos = bounds.center + Vector3.down * bounds.extents.y;
+
+			// Set the reach target to be within grabbing distance to the target
+			var curReachTarget = setDownPos + Vector3.up * bounds.size.y - holdOffset;
+
+			DataStore.SetValue("me:intent:target", new DataStore.Vector3Value(curReachTarget), null, "");
+			doGraspRandomObject = false;
+		}
+
+		else if (doMoveRandomLocation)
+		{
+			DataStore.SetValue("me:intent:action", new DataStore.StringValue("move"), null, "");
+			Vector3 v = GetRandomLocation();
+			DataStore.SetValue("me:intent:target", new DataStore.Vector3Value(v), null, "");
+			doMoveRandomLocation = false;
+		}
+
+		else if (doUngrasp)
+		{
+			DataStore.SetValue("me:intent:action", new DataStore.StringValue("ungrasp"), null, "");
+			doUngrasp = false;
+		}
+    }
+}
