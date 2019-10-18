6a67b553e48a5a96a074b510e7ae53a38ac22cf1
Rahul Bangar <bangar1991@gmail.com>
2019-10-18 17:41:09 -0600

    Add ServoBack state machine and update ServoIntentModule

---
+++ BlocksWorld/Assets/_Core/Scripts/CogArch/StateMachines/ServoBackStateMachine.cs
@@ -0,0 +1,38 @@
+﻿public enum ServoBackState
+{
+	ServoBackStop,
+	ServoBackStart
+}
+
+public class ServoBackStateMachine : RuleStateMachine<ServoBackState>
+{
+	public ServoBackStateMachine()
+	{
+		SetTransitionRule(ServoBackState.ServoBackStop, ServoBackState.ServoBackStart, new TimedRule(() =>
+		{
+			bool userIsEngaged = DataStore.GetBoolValue("user:isEngaged");
+
+			if (userIsEngaged)
+			{
+				string rightHandGesture = DataStore.GetStringValue("user:hands:right");
+				string leftHandGesture = DataStore.GetStringValue("user:hands:left");
+				return rightHandGesture == "beckon" || leftHandGesture == "beckon";
+			}
+			return false;
+		}, 300));
+
+		SetTransitionRule(ServoBackState.ServoBackStart, ServoBackState.ServoBackStop, new TimedRule(() =>
+		{
+			bool userIsEngaged = DataStore.GetBoolValue("user:isEngaged");
+
+			if (!userIsEngaged)
+				return true;
+			else
+			{
+				string rightHandGesture = DataStore.GetStringValue("user:hands:right");
+				string leftHandGesture = DataStore.GetStringValue("user:hands:left");
+				return rightHandGesture != "beckon" && leftHandGesture != "beckon";
+			}
+		}, 100));
+	}
+}
