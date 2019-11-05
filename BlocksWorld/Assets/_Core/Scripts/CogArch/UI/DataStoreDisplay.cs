9ad1450d51be008e806d76db49fa84d04207d184
JoeStrout <joe@strout.net>
2019-11-05 14:16:13 -0700

    Add Filter properties to Data Store Display.

--- BlocksWorld/Assets/_Core/Scripts/CogArch/UI/DataStoreDisplay.cs
+++ BlocksWorld/Assets/_Core/Scripts/CogArch/UI/DataStoreDisplay.cs
@@ -8,14 +8,29 @@ for development and debugging.  To use:
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using TMPro;
 
 public class DataStoreDisplay : MonoBehaviour {
 
+	[Header("Filter")]
+	
+	[Tooltip("Glob patterns for keys to display, e.g. user.*; or blank for all keys")]
+	public string keysToShow;
+	
+	[Tooltip("Glob patterns for keys to hide, e.g. user.*; or blank to hide none")]
+	public string keysToHide = "user:joint:*";
+
 	TextMeshProUGUI outputText;
 	
 	bool needsUpdate = true;
+	
+	Regex keysToShowPat;
+	string _keysToShow;		// value of keysToShow when keysToShowPat was last updated
+	
+	Regex keysToHidePat;
+	string _keysToHide;		// value of keysToHide when keysToHidePat was last updated
 	
 	Dictionary<string, string> kvData = new Dictionary<string, string>();
 	
@@ -40,8 +55,7 @@ public class DataStoreDisplay : MonoBehaviour {
 		var sb = new System.Text.StringBuilder();
 		sb.Append("<margin-left=10em><line-indent=-10em>");
 		foreach (string k in keys) {
-            if (!k.StartsWith("user:joint"))
-            {
+			if (ShouldShow(k)) {
                 sb.Append(k);
                 sb.Append(" = ");
                 sb.Append(kvData[k]);
@@ -49,5 +63,55 @@ public class DataStoreDisplay : MonoBehaviour {
             }
 		}
 		outputText.text = sb.ToString();
+	}
+	
+	/// <summary>
+	/// Determine whether we should show the given key, based on our 
+	/// keysToShow and keysToHide values.
+	/// </summary>
+	/// <param name="key"></param>
+	/// <returns></returns>
+	bool ShouldShow(string key) {
+		if (!string.IsNullOrEmpty(keysToShow)) {
+			// If it doesn't match some pattern in keysToShow, then don't show it.
+			if (keysToShow != _keysToShow) {
+				try {
+					keysToShowPat = GlobPatternToRegex(keysToShow);
+					_keysToShow = keysToShow;
+				} catch {}
+			}
+			if (!keysToShowPat.IsMatch(key)) return false;
+		}
+		
+		if (!string.IsNullOrEmpty(keysToHide)) {
+			// If it does match any pattern in keysToHide, then don't show it.
+			if (keysToHide != _keysToHide) {
+				try {
+					keysToHidePat = GlobPatternToRegex(keysToHide);
+					_keysToHide = keysToHide;
+				} catch {}
+			}
+			if (keysToHidePat.IsMatch(key)) return false;			
+		}
+		
+		// Otherwise, show it!
+		return true;
+	}
+	
+	/// <summary>
+	/// Convert a glob pattern to a regular expression.
+	/// Example:   "foo:*;bar:baz:*"    ->	  "^(foo:.*|bar:baz:.*)$"
+	/// </summary>
+	/// <param name="pattern"></param>
+	/// <returns></returns>
+	Regex GlobPatternToRegex(string pattern) {
+		string pat = pattern.Replace(".", "\\.");
+		pat = pat.Replace(";", "|");
+		pat = pat.Replace("*", ".*");
+		pat = pat.Replace("?", ".");
+		pat = "^(" + pat + ")$";		// (anchor at start and end)
+		Debug.Log("Translating glob \"" + pattern + "\" to Regex: " + pat);
+		Regex re = new Regex(pat);
+		return re;
 	}
 }
